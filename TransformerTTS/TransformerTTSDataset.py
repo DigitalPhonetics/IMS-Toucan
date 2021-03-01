a9d0426402e8b85b9c9a59473bfc9fb604c6afc9
FlorianLux <florian.lux@ims.uni-stuttgart.de>
2021-03-01 12:57:07 +0100

    fix multiprocessed dataloading

--- TransformerTTS/TransformerTTSDataset.py
+++ TransformerTTS/TransformerTTSDataset.py
@@ -12,7 +12,7 @@ from PreprocessingForTTS.ProcessText import TextFrontend
 class TransformerTTSDataset(Dataset):
 
     def __init__(self, path_to_transcript_dict, device=torch.device("cpu"), spemb=False, train=True,
-                 loading_processes=16):
+                 loading_processes=40):
         ressource_manager = Manager()
         self.path_to_transcript_dict = ressource_manager.dict(path_to_transcript_dict)
         if train:
@@ -21,16 +21,7 @@ class TransformerTTSDataset(Dataset):
             key_list = list(self.path_to_transcript_dict.keys())[-100:]
         self.spemb = spemb
         self.device = device
-        self.tf = TextFrontend(language="de",
-                               use_panphon_vectors=False,
-                               use_shallow_pos=False,
-                               use_sentence_type=False,
-                               use_positional_information=False,
-                               use_word_boundaries=False,
-                               use_chinksandchunks_ipb=False,
-                               use_explicit_eos=True)
-        _, sr = sf.read(os.path.join("Corpora/CSS10/", key_list[0]))
-        self.ap = AudioPreprocessor(input_sr=sr, output_sr=16000, melspec_buckets=80, hop_length=256, n_fft=1024)
+
         # build cache
         print("... building dataset cache ...")
         self.datapoints = ressource_manager.list()
@@ -39,7 +30,7 @@ class TransformerTTSDataset(Dataset):
         process_list = list()
         for i in range(loading_processes):
             key_splits.append(
-                key_list[i * len(key_list) // loading_processes:i + 1 * len(key_list) // loading_processes])
+                key_list[i * len(key_list) // loading_processes:(i + 1) * len(key_list) // loading_processes])
         for key_split in key_splits:
             process_list.append(Process(target=self.cache_builder_process, args=(key_split,)))
             process_list[-1].start()
@@ -47,14 +38,24 @@ class TransformerTTSDataset(Dataset):
             process.join()
 
     def cache_builder_process(self, path_list):
+        tf = TextFrontend(language="de",
+                          use_panphon_vectors=False,
+                          use_shallow_pos=False,
+                          use_sentence_type=False,
+                          use_positional_information=False,
+                          use_word_boundaries=False,
+                          use_chinksandchunks_ipb=False,
+                          use_explicit_eos=True)
+        _, sr = sf.read(os.path.join("Corpora/CSS10/", path_list[0]))
+        ap = AudioPreprocessor(input_sr=sr, output_sr=16000, melspec_buckets=80, hop_length=256, n_fft=1024)
         for path in path_list:
             transcript = self.path_to_transcript_dict[path]
             wave, _ = sf.read(os.path.join("Corpora/CSS10/", path))
             if 50000 < len(wave) < 230000:
                 print("processing {}".format(path))
-                cached_text = self.tf.string_to_tensor(transcript).long()
+                cached_text = tf.string_to_tensor(transcript).long()
                 cached_text_lens = torch.LongTensor([len(cached_text)])
-                cached_speech = self.ap.audio_to_mel_spec_tensor(wave).transpose(0, 1)
+                cached_speech = ap.audio_to_mel_spec_tensor(wave).transpose(0, 1)
                 cached_speech_lens = torch.LongTensor([len(cached_speech)])
                 self.datapoints.append([cached_text, cached_text_lens, cached_speech, cached_speech_lens])
                 if self.spemb:
