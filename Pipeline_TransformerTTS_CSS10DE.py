79e598283d0d31a8dec15c66f2c9a8d9dcc895ae
FlorianLux <florian.lux@ims.uni-stuttgart.de>
2021-03-08 15:34:40 +0100

    prepare for new feature cache files

--- Pipeline_TransformerTTS_CSS10DE.py
+++ Pipeline_TransformerTTS_CSS10DE.py
@@ -59,7 +59,8 @@ if __name__ == '__main__':
     train_loop(net=model,
                train_dataset=train_set,
                eval_dataset=valid_set,
-               device=torch.device("cuda:7"),
+               # device=torch.device("cuda:7"),
+               device=torch.device("cpu"),
                config=model.get_conf(),
                save_directory=save_dir,
                epochs=300000,  # just kill the process at some point
