efc4a9d83c0655273973c217ba4ad5def772e2d8
FlorianLux <florian.lux@ims.uni-stuttgart.de>
2021-03-10 16:29:17 +0100

    fix wrong device

--- Pipeline_TransformerTTS_CSS10DE.py
+++ Pipeline_TransformerTTS_CSS10DE.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
     train_loop(net=model,
                train_dataset=train_set,
                valid_dataset=valid_set,
-               device=torch.device("cuda:7"),
+               device=torch.device("cuda:1"),
                config=model.get_conf(),
                save_directory=save_dir,
                epochs=300000,  # just kill the process at some point
