hopefully actually fix gradient scaling with gradient accumulation

TransformerTTS/transformer_tts_train_loop.py
@@ -187,7 +187,35 @@ def train_loop(net, train_dataset, valid_dataset, device, save_directory,
         optimizer.zero_grad()
         train_losses_this_epoch = list()
         for train_datapoint in train_loader:
-            with autocast():
+            if gradient_accumulation == 1:
+                with autocast():
+                    if not spemb:
+                        train_loss = net(train_datapoint[0].to(device),
+                                         train_datapoint[1].to(device),
+                                         train_datapoint[2].to(device),
+                                         train_datapoint[3].to(device))
+                    else:
+                        train_loss = net(train_datapoint[0].to(device),
+                                         train_datapoint[1].to(device),
+                                         train_datapoint[2].to(device),
+                                         train_datapoint[3].to(device),
+                                         train_datapoint[4].to(device))
+                    train_losses_this_epoch.append(float(train_loss))
+                scaler.scale(train_loss / gradient_accumulation).backward()
+                del train_loss
+                grad_accum += 1
+                if grad_accum % gradient_accumulation == 0:
+                    grad_accum = 0
+                    step_counter += 1
+                    # update weights
+                    # print("Step: {}".format(step_counter))
+                    torch.nn.utils.clip_grad_norm_(net.parameters(), 1.0)
+                    scaler.step(optimizer)
+                    scaler.update()
+                    scheduler.step()
+                    optimizer.zero_grad()
+                    torch.cuda.empty_cache()
+            else:
                 if not spemb:
                     train_loss = net(train_datapoint[0].to(device),
                                      train_datapoint[1].to(device),
@@ -200,20 +228,20 @@ def train_loop(net, train_dataset, valid_dataset, device, save_directory,
                                      train_datapoint[3].to(device),
                                      train_datapoint[4].to(device))
                 train_losses_this_epoch.append(float(train_loss))
-            scaler.scale(train_loss / gradient_accumulation).backward()
-            del train_loss
-            grad_accum += 1
-            if grad_accum % gradient_accumulation == 0:
-                grad_accum = 0
-                step_counter += 1
-                # update weights
-                # print("Step: {}".format(step_counter))
-                torch.nn.utils.clip_grad_norm_(net.parameters(), 1.0)
-                scaler.step(optimizer)
-                scaler.update()
-                scheduler.step()
-                optimizer.zero_grad()
-                torch.cuda.empty_cache()
+                (train_loss / gradient_accumulation).backward()
+                del train_loss
+                grad_accum += 1
+                if grad_accum % gradient_accumulation == 0:
+                    grad_accum = 0
+                    step_counter += 1
+                    # update weights
+                    # print("Step: {}".format(step_counter))
+                    torch.nn.utils.clip_grad_norm_(net.parameters(), 1.0)
+                    optimizer.step()
+                    scheduler.step()
+                    optimizer.zero_grad()
+                    torch.cuda.empty_cache()
+
         # evaluate on valid after every epoch
         with torch.no_grad():
             net.eval()