b49a540fd0093a30a5723e197870b19cd9f14d4d
Florian Lux <florian.lux@ims.uni-stuttgart.de>
2021-03-30 12:14:09 +0200

    fix error in path to transcript dict

--- Utility/path_to_transcript_dicts.py
+++ Utility/path_to_transcript_dicts.py
@@ -58,5 +58,5 @@ def build_path_to_transcript_dict_css10de():
     trans_lines = transcriptions.split("\n")
     for line in trans_lines:
         if line.strip() != "":
-            path_to_transcript["Corpora/CSS10_DE/" + line.split("|")[0]] = line.split("|")[2]
+            path_to_transcript["/mount/resources/speech/corpora/CSS10/german/" + line.split("|")[0]] = line.split("|")[2]
     return path_to_transcript
