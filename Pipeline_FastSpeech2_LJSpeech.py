a2a05ccef642bbba1af65d1f9564c89aaf776ef6
Flux9665 <florian.lux@ims.uni-stuttgart.de>
2021-04-01 20:27:30 +0200

    use correct gpu

--- Pipeline_FastSpeech2_LJSpeech.py
+++ Pipeline_FastSpeech2_LJSpeech.py
@@ -7,7 +7,7 @@ This requires having a trained TransformerTTS model in the right directory to kn
 import os
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+os.environ["CUDA_VISIBLE_DEVICES"] = "4"
 import random
 import warnings
 
