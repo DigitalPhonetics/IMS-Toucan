f074571271b9d16e139d394cc8643084d5666846
Flux9665 <florian.lux@ims.uni-stuttgart.de>
2021-04-04 15:19:05 +0200

    implement visualization in demo

--- Demo.py
+++ Demo.py
@@ -32,4 +32,4 @@ if __name__ == '__main__':
         text = input("\nWhat should I say? (or 'exit')\n")
         if text == "exit":
             sys.exit()
-        tts.read_aloud(text)
+        tts.read_aloud(text, view=True)
