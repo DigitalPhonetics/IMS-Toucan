332861f21846fea01e4307c1e81c58c4ec33b545
Flux9665 <florian.lux@ims.uni-stuttgart.de>
2021-04-04 14:36:56 +0200

    rework text reader to function with new structure

--- TextReader.py
+++ TextReader.py
@@ -2,11 +2,31 @@ import os
 
 import torch
 
-from InferenceInterfaces.SingleSpeakerTransformerTTSInference import SingleSpeakerTransformerTTSInference
+from InferenceInterfaces.CSS10_DE_FastSpeechInference import CSS10_DE_FastSpeechInference
+from InferenceInterfaces.CSS10_DE_TransformerTTSInference import CSS10_DE_TransformerTTSInference
+from InferenceInterfaces.LJSpeech_FastSpeechInference import LJSpeech_FastSpeechInference
+from InferenceInterfaces.LJSpeech_TransformerTTSInference import LJSpeech_TransformerTTSInference
+from InferenceInterfaces.LibriTTS_FastSpeechInference import LibriTTS_FastSpeechInference
+from InferenceInterfaces.LibriTTS_TransformerTTSInference import LibriTTS_TransformerTTSInference
+from InferenceInterfaces.Thorsten_FastSpeechInference import Thorsten_FastSpeechInference
+from InferenceInterfaces.Thorsten_TransformerTTSInference import Thorsten_TransformerTTSInference
+
+tts_dict = {
+    "fast_thorsten": Thorsten_FastSpeechInference,
+    "fast_lj": LJSpeech_FastSpeechInference,
+    "fast_css10_de": CSS10_DE_FastSpeechInference,
+    "fast_libri": LibriTTS_FastSpeechInference,
+
+    "trans_thorsten": Thorsten_TransformerTTSInference,
+    "trans_lj": LJSpeech_TransformerTTSInference,
+    "trans_css10_de": CSS10_DE_TransformerTTSInference,
+    "trans_libri": LibriTTS_TransformerTTSInference
+
+}
 
 
-def read_texts(lang, sentence, filename, reduction_factor=1, device="cpu"):
-    tts = SingleSpeakerTransformerTTSInference(lang=lang, reduction_factor=reduction_factor, device=device)
+def read_texts(model_id, sentence, filename, device="cpu"):
+    tts = tts_dict[model_id](device=device)
     if type(sentence) == str:
         sentence = [sentence]
     tts.read_to_file(text_list=sentence, file_location=filename)
@@ -18,16 +38,7 @@ if __name__ == '__main__':
     if not os.path.isdir("audios"):
         os.makedirs("audios")
 
-    read_texts(lang="de",
+    read_texts(model_id="trans_thorsten",
                sentence=["Im Frühling denkt das Rößlein, wer nicht leiden will, muss schön sein!"],
-               filename="audios/test_de.wav",
-               reduction_factor=1,
-               device=device)
-
-    read_texts(lang="en",
-               sentence=["Lying in a field of glass, underneath the overpass.",
-                         "Mangled in the shards of a metal frame.",
-                         "Woken from the dream by my own name."],
-               filename="audios/test_en.wav",
-               reduction_factor=1,
+               filename="audios/thorsten.wav",
                device=device)
