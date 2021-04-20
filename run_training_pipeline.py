d7e23abeb4cbf5b0df4829d6dc913e910ba55d94
Flux9665 <florian.lux@ims.uni-stuttgart.de>
2021-04-20 18:18:08 +0200

    add recipe for fastspeech with different hyperparams

--- run_training_pipeline.py
+++ run_training_pipeline.py
@@ -5,6 +5,7 @@ from Pipelines.Pipeline_FastSpeech2_Elizabeth import run as fast_Elizabeth
 from Pipelines.Pipeline_FastSpeech2_Eva import run as fast_Eva
 from Pipelines.Pipeline_FastSpeech2_Karlsson import run as fast_Karlsson
 from Pipelines.Pipeline_FastSpeech2_LJSpeech import run as fast_LJSpeech
+from Pipelines.Pipeline_FastSpeech2_LJSpeech_new_hyp import run as fast_lj_hyp
 from Pipelines.Pipeline_FastSpeech2_LibriTTS import run as fast_LibriTTS
 from Pipelines.Pipeline_FastSpeech2_Thorsten import run as fast_Thorsten
 from Pipelines.Pipeline_MelGAN_Elizabeth import run as melgan_Elizabeth
@@ -21,30 +22,32 @@ from Pipelines.Pipeline_TransformerTTS_LibriTTS import run as trans_LibriTTS
 from Pipelines.Pipeline_TransformerTTS_Thorsten import run as trans_Thorsten
 
 pipeline_dict = {
-    "fast_Eva"        : fast_Eva,
-    "melgan_Eva"      : melgan_Eva,
-    "trans_Eva"       : trans_Eva,
+    "fast_Eva": fast_Eva,
+    "melgan_Eva": melgan_Eva,
+    "trans_Eva": trans_Eva,
 
-    "fast_Karlsson"   : fast_Karlsson,
-    "melgan_Karlsson" : melgan_Karlsson,
-    "trans_Karlsson"  : trans_Karlsson,
+    "fast_Karlsson": fast_Karlsson,
+    "melgan_Karlsson": melgan_Karlsson,
+    "trans_Karlsson": trans_Karlsson,
 
-    "fast_Elizabeth"  : fast_Elizabeth,
+    "fast_Elizabeth": fast_Elizabeth,
     "melgan_Elizabeth": melgan_Elizabeth,
-    "trans_Elizabeth" : trans_Elizabeth,
+    "trans_Elizabeth": trans_Elizabeth,
 
-    "fast_Thorsten"   : fast_Thorsten,
-    "melgan_Thorsten" : melgan_Thorsten,
-    "trans_Thorsten"  : trans_Thorsten,
+    "fast_Thorsten": fast_Thorsten,
+    "melgan_Thorsten": melgan_Thorsten,
+    "trans_Thorsten": trans_Thorsten,
 
-    "fast_LibriTTS"   : fast_LibriTTS,
-    "melgan_LibriTTS" : melgan_LibriTTS,
-    "trans_LibriTTS"  : trans_LibriTTS,
+    "fast_LibriTTS": fast_LibriTTS,
+    "melgan_LibriTTS": melgan_LibriTTS,
+    "trans_LibriTTS": trans_LibriTTS,
 
-    "fast_LJSpeech"   : fast_LJSpeech,
-    "melgan_LJSpeech" : melgan_LJSpeech,
-    "trans_LJSpeech"  : trans_LJSpeech,
-    }
+    "fast_LJSpeech": fast_LJSpeech,
+    "melgan_LJSpeech": melgan_LJSpeech,
+    "trans_LJSpeech": trans_LJSpeech,
+
+    "fast_lj_hyp": fast_lj_hyp
+}
 
 parser = argparse.ArgumentParser(description='IMS Speech Synthesis Toolkit - Call to Train')
 
