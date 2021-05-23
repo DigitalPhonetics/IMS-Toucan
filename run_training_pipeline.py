a37b9d92a8e444a3acbcebe4f82324e129ffb87e
Flux9665 <florian.lux@ims.uni-stuttgart.de>
2021-05-23 02:17:55 +0200

    "If using force does not solve your problems, you're not using enough force."

--- run_training_pipeline.py
+++ run_training_pipeline.py
@@ -60,7 +60,7 @@ pipeline_dict = {
 
 if __name__ == '__main__':
 
-    torch.multiprocessing.set_start_method('spawn')
+    torch.multiprocessing.set_start_method('spawn', force=True)
 
     parser = argparse.ArgumentParser(description='IMS Speech Synthesis Toolkit - Call to Train')
 
