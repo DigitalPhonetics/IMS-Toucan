20c78f2ccaabaebd960c35aa5943db0136216772
Florian Lux <florian.lux@ims.uni-stuttgart.de>
2021-06-08 02:09:15 +0200

    update paths for LibriTTS

--- Utility/file_lists.py
+++ Utility/file_lists.py
@@ -63,7 +63,7 @@ def get_file_list_nancy():
 
 
 def get_file_list_libritts():
-    path_train = "/mount/resources/speech/corpora/LibriTTS/train-clean-100"
+    path_train = "/mount/resources/speech/corpora/LibriTTS"
     file_list = list()
     for speaker in os.listdir(path_train):
         for chapter in os.listdir(os.path.join(path_train, speaker)):
