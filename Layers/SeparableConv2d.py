958018d7e3b38dea3e4b21f9534250ee8b48af25
ac130030 <florian.lux@ims.uni-stuttgart.de>
2021-06-08 20:40:38 +0200

    Update SeparableConv2d.py

--- Layers/SeparableConv2d.py
+++ Layers/SeparableConv2d.py
@@ -1,15 +1,12 @@
 import torch.nn as nn
 
-
-class SeparableConv2d(nn.Module):
-
-    def __init__(self, in_channels, out_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=False):
-        super(SeparableConv2d, self).__init__()
-
-        self.conv1 = nn.Conv2d(in_channels, in_channels, kernel_size, stride, padding, dilation, groups=in_channels, bias=bias)
-        self.pointwise = nn.Conv2d(in_channels, out_channels, 1, 1, 0, 1, 1, bias=bias)
+class DepthwiseSeparableConvolution(nn.Module):
+    def __init__(self, n_in, n_out, kernel_size = 3, padding = 1, bias=False):
+        super(DepthwiseSeparableConvolution, self).__init__()
+        self.depthwise = nn.Conv2d(n_in, n_in, kernel_size=kernel_size, padding=padding, groups=n_in, bias=bias)
+        self.pointwise = nn.Conv2d(n_in, n_out, kernel_size=1, bias=bias)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.pointwise(x)
-        return x
+        out = self.depthwise(x)
+        out = self.pointwise(out)
+        return out
