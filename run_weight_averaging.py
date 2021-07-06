1a4f311cf5b1be6afc87be4b4526f45e398e3c8e
Florian Lux <florian.lux@ims.uni-stuttgart.de>
2021-07-06 18:25:31 +0200

    fix legacy models for weight averaging

--- run_weight_averaging.py
+++ run_weight_averaging.py
@@ -36,14 +36,22 @@ def load_net_fast(path, idim=166, odim=80):
 def load_net_trans_multi(path, idim=166, odim=80):
     check_dict = torch.load(path, map_location=torch.device("cpu"))
     net = Transformer(idim=idim, odim=odim, spk_embed_dim=256)
-    net.load_state_dict(check_dict["model"])
+    try:
+        net.load_state_dict(check_dict["model"])
+    except RuntimeError:
+        net = FastSpeech2(idim=idim, odim=odim, spk_embed_dim=256, legacy_model=True)
+        net.load_state_dict(check_dict["model"])
     return net
 
 
 def load_net_fast_multi(path, idim=166, odim=80):
     check_dict = torch.load(path, map_location=torch.device("cpu"))
     net = FastSpeech2(idim=idim, odim=odim, spk_embed_dim=256)
-    net.load_state_dict(check_dict["model"])
+    try:
+        net.load_state_dict(check_dict["model"])
+    except RuntimeError:
+        net = FastSpeech2(idim=idim, odim=odim, spk_embed_dim=256, legacy_model=True)
+        net.load_state_dict(check_dict["model"])
     return net
 
 
