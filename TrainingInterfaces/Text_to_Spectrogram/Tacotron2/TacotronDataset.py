a0bb85b487ae53bd79cb1469f67fa3c950ea53c5
Florian Lux <florian.lux@ims.uni-stuttgart.de>
2021-09-25 17:37:21 +0200

    fix slight inefficiency in dataset

--- TrainingInterfaces/Text_to_Spectrogram/Tacotron2/TacotronDataset.py
+++ TrainingInterfaces/Text_to_Spectrogram/Tacotron2/TacotronDataset.py
@@ -123,6 +123,10 @@ class TacotronDataset(Dataset):
         for path in tqdm(path_list):
             transcript = self.path_to_transcript_dict[path]
             wave, sr = sf.read(path)
+            dur_in_seconds = len(wave) / sr
+            if not (min_len <= dur_in_seconds <= max_len):
+                print(f"Excluding {path} because of its duration of {round(dur_in_seconds, 2)} seconds.")
+                continue
             if sr != ap.sr:
                 print(f"Inconsistent sampling rate in the Data! Excluding {path}")
                 continue
@@ -133,24 +137,24 @@ class TacotronDataset(Dataset):
             dur_in_seconds = len(norm_wave) / 16000
             if not (min_len <= dur_in_seconds <= max_len):
                 print(f"Excluding {path} because of its duration of {round(dur_in_seconds, 2)} seconds.")
+                continue
+            cached_text = tf.string_to_tensor(transcript).squeeze(0).cpu().numpy()
+            cached_text_len = torch.LongTensor([len(cached_text)]).numpy()
+            cached_speech = ap.audio_to_mel_spec_tensor(audio=norm_wave, normalize=False).transpose(0, 1).cpu().numpy()
+            cached_speech_len = torch.LongTensor([len(cached_speech)]).numpy()
+            if speaker_embedding:
+                process_internal_dataset_chunk.append([cached_text,
+                                                       cached_text_len,
+                                                       cached_speech,
+                                                       cached_speech_len,
+                                                       norm_wave.numpy(),
+                                                       norm_wave.cpu().detach().numpy()])
             else:
-                cached_text = tf.string_to_tensor(transcript).squeeze(0).cpu().numpy()
-                cached_text_len = torch.LongTensor([len(cached_text)]).numpy()
-                cached_speech = ap.audio_to_mel_spec_tensor(audio=norm_wave, normalize=False).transpose(0, 1).cpu().numpy()
-                cached_speech_len = torch.LongTensor([len(cached_speech)]).numpy()
-                if speaker_embedding:
-                    process_internal_dataset_chunk.append([cached_text,
-                                                           cached_text_len,
-                                                           cached_speech,
-                                                           cached_speech_len,
-                                                           norm_wave.numpy(),
-                                                           norm_wave.cpu().detach().numpy()])
-                else:
-                    process_internal_dataset_chunk.append([cached_text,
-                                                           cached_text_len,
-                                                           cached_speech,
-                                                           cached_speech_len,
-                                                           norm_wave.cpu().detach().numpy()])
+                process_internal_dataset_chunk.append([cached_text,
+                                                       cached_text_len,
+                                                       cached_speech,
+                                                       cached_speech_len,
+                                                       norm_wave.cpu().detach().numpy()])
         self.datapoints += process_internal_dataset_chunk
 
     def __getitem__(self, index):
