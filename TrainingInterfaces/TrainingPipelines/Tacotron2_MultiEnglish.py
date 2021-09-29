c0862e818fbc62235af58343bd6b2a275fbc7cfd
Flux9665 <florian.lux@ims.uni-stuttgart.de>
2021-09-29 14:19:29 +0200

    further update libritts batchsize

--- TrainingInterfaces/TrainingPipelines/Tacotron2_MultiEnglish.py
+++ TrainingInterfaces/TrainingPipelines/Tacotron2_MultiEnglish.py
@@ -48,7 +48,7 @@ def run(gpu_id, resume_checkpoint, finetune, model_dir):
                device=device,
                save_directory=save_dir,
                steps=100000,
-               batch_size=64,
+               batch_size=84,
                epochs_per_save=1,
                use_speaker_embedding=True,
                lang="en",
