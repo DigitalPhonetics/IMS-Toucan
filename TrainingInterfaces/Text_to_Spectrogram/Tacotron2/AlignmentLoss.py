d78c9a4b33491d78068a75c585b93c0f4c2e2f1c
Florian Lux <florian.lux@ims.uni-stuttgart.de>
2021-09-29 03:49:45 +0200

    prepare experiment with location attention for libritts

--- TrainingInterfaces/Text_to_Spectrogram/Tacotron2/AlignmentLoss.py
+++ TrainingInterfaces/Text_to_Spectrogram/Tacotron2/AlignmentLoss.py
@@ -170,7 +170,7 @@ class AlignmentLoss(nn.Module):
     def forward(self, soft_attention, in_lens, out_lens, step):
 
         soft_attention = soft_attention.unsqueeze(1)
-        bin_weight = min(((step - self.bin_start_steps) / self.bin_warmup_steps) / 2, 0.5)
+        bin_weight = min(((step - self.bin_start_steps) / self.bin_warmup_steps) / 10, 0.1)
 
         if self.include_forward_loss:
             l_forward = self.l_forward_func(torch.log(soft_attention), in_lens, out_lens) / 3
