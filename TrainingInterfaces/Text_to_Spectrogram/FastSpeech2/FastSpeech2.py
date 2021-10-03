c6f0480caba2bdaa25d683719ea3293ebef698aa
Flux9665 <florian.lux@ims.uni-stuttgart.de>
2021-10-03 12:52:51 +0200

    adapt speaker embedding projection size also in fastspeech (multispeaker tacotron works as of this commit)

--- TrainingInterfaces/Text_to_Spectrogram/FastSpeech2/FastSpeech2.py
+++ TrainingInterfaces/Text_to_Spectrogram/FastSpeech2/FastSpeech2.py
@@ -100,7 +100,8 @@ class FastSpeech2(torch.nn.Module, ABC):
                  use_weighted_masking=True,
                  # additional features
                  legacy_model=False,
-                 use_dtw_loss=False):
+                 use_dtw_loss=False,
+                 speaker_embedding_projection_size=64):
         super().__init__()
 
         # store hyperparameters
@@ -129,9 +130,9 @@ class FastSpeech2(torch.nn.Module, ABC):
 
         # define additional projection for speaker embedding
         if spk_embed_dim is not None:
-            self.hs_emb_projection = torch.nn.Linear(adim + 256, adim)
+            self.hs_emb_projection = torch.nn.Linear(adim + speaker_embedding_projection_size, adim)
             # embedding projection derived from https://arxiv.org/pdf/1705.08947.pdf
-            self.embedding_projection = torch.nn.Sequential(torch.nn.Linear(spk_embed_dim, 256),
+            self.embedding_projection = torch.nn.Sequential(torch.nn.Linear(spk_embed_dim, speaker_embedding_projection_size),
                                                             torch.nn.Softsign())
 
         # define duration predictor
