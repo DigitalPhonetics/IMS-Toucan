4594a20458aa6e8ef8e445accc19ef152774687a
Florian Lux <florian.lux@ims.uni-stuttgart.de>
2021-10-06 21:40:25 +0200

    further debug freezing issue

--- Layers/STFT.py
+++ Layers/STFT.py
@@ -61,10 +61,18 @@ class STFT(torch.nn.Module):
             window = None
 
         print("ok now I will stft for real")
-        output = torch.view_as_real(
-            torch.stft(input_wave, n_fft=self.n_fft, win_length=self.win_length, hop_length=self.hop_length, center=self.center, window=window,
-                       normalized=self.normalized, onesided=self.onesided, return_complex=True))
+        complex_output = torch.stft(input=input_wave,
+                                    n_fft=self.n_fft,
+                                    win_length=self.win_length,
+                                    hop_length=self.hop_length,
+                                    center=self.center,
+                                    window=window,
+                                    normalized=self.normalized,
+                                    onesided=self.onesided,
+                                    return_complex=True)
         print("I have stftd")
+        output = torch.view_as_real(complex_output)
+        print("I am now viewing as real")
         # output: (Batch, Freq, Frames, 2=real_imag)
         # -> (Batch, Frames, Freq, 2=real_imag)
         output = output.transpose(1, 2)
