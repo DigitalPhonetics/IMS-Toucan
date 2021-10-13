ec8e5b4697ff01a1a6e0c709eeb0b97d8727e3f5
Florian Lux <florian.lux@ims.uni-stuttgart.de>
2021-10-14 00:24:52 +0200

    prepare for multispeaker aligner experiment

--- TrainingInterfaces/Text_to_Spectrogram/Tacotron2/AlignmentLoss.py
+++ TrainingInterfaces/Text_to_Spectrogram/Tacotron2/AlignmentLoss.py
@@ -169,8 +169,8 @@ class AlignmentLoss(nn.Module):
     def __init__(self,
                  bin_warmup_steps=10000,
                  bin_start_steps=60000,
-                 forward_start_steps=5000,
-                 forward_warmup_steps=5000,
+                 forward_start_steps=500,
+                 forward_warmup_steps=500,
                  include_forward_loss=True):
         super().__init__()
         if include_forward_loss:
