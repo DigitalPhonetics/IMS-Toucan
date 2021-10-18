ec3b856f8338796b4bc01b288157f491ede61d53
Flux9665 <florian.lux@ims.uni-stuttgart.de>
2021-10-18 17:43:07 +0200

    prepare for run on different server

--- TrainingInterfaces/TrainingPipelines/Tacotron2_MetaCheckpoint_automatic_process_spawning.py
+++ TrainingInterfaces/TrainingPipelines/Tacotron2_MetaCheckpoint_automatic_process_spawning.py
@@ -147,7 +147,7 @@ def run(gpu_id, resume_checkpoint, finetune, model_dir, resume):
                                     max_len_in_seconds=13))
 
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-    gpus_usable = ["0", "1", "2", "3"]
+    gpus_usable = ["4", "5", "6", "7", "8"]
     os.environ["CUDA_VISIBLE_DEVICES"] = "{}".format(",".join(gpus_usable))
     gpus_available = list(range(len(gpus_usable)))
     gpus_in_use = []
