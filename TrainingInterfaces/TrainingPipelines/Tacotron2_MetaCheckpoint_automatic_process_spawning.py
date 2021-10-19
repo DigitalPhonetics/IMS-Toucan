6582144302552938febb32eaa60d67d35bd51b3f
Flux9665 <florian.lux@ims.uni-stuttgart.de>
2021-10-19 16:04:05 +0200

    fix GPU ids

--- TrainingInterfaces/TrainingPipelines/Tacotron2_MetaCheckpoint_automatic_process_spawning.py
+++ TrainingInterfaces/TrainingPipelines/Tacotron2_MetaCheckpoint_automatic_process_spawning.py
@@ -147,8 +147,8 @@ def run(gpu_id, resume_checkpoint, finetune, model_dir, resume):
                                     max_len_in_seconds=13))
 
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-    gpus_usable = ["4", "5", "6", "7", "8", "4", "6", "7", "8"]
-    os.environ["CUDA_VISIBLE_DEVICES"] = "{}".format(",".join(list(set(gpus_usable))))
+    gpus_usable = ["4", "5", "6", "7", "8"]
+    os.environ["CUDA_VISIBLE_DEVICES"] = "{}".format(",".join(gpus_usable))
     gpus_available = list(range(len(gpus_usable)))
     gpus_in_use = []
 
