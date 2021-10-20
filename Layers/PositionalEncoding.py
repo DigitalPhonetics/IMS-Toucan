669f09ba351e13465d9416d7d8077fe8fed11e87
Flux9665 <florian.lux@ims.uni-stuttgart.de>
2021-10-20 17:58:14 +0200

    more debug statements

--- Layers/PositionalEncoding.py
+++ Layers/PositionalEncoding.py
@@ -28,7 +28,7 @@ class PositionalEncoding(torch.nn.Module):
         self.xscale = math.sqrt(self.d_model)
         self.dropout = torch.nn.Dropout(p=dropout_rate)
         self.pe = None
-        self.extend_pe(torch.tensor(0.0).expand(1, max_len))
+        self.extend_pe(torch.tensor(0.0, device=d_model.device).expand(1, max_len))
 
     def extend_pe(self, x):
         """
@@ -100,38 +100,27 @@ class RelPositionalEncoding(torch.nn.Module):
         # position of key vector. We use position relative positions when keys
         # are to the left (i>j) and negative relative positions otherwise (i<j).
 
-        print("extending positional encoding")
-
-        pe_positive = torch.zeros(x.size(1), self.d_model, device=x.device, dtype=x.dtype)
-        pe_negative = torch.zeros(x.size(1), self.d_model, device=x.device, dtype=x.dtype)
-
-        print("created zeros")
+        pe_positive = torch.zeros(x.size(1), self.d_model, device=x.device)
+        pe_negative = torch.zeros(x.size(1), self.d_model, device=x.device)
 
         position = torch.arange(0, x.size(1), dtype=torch.float32, device=x.device).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, self.d_model, 2, dtype=torch.float32, device=x.device) * -(math.log(10000.0) / self.d_model))
-
-        print("positionified")
 
         pe_positive[:, 0::2] = torch.sin(position * div_term)
         pe_positive[:, 1::2] = torch.cos(position * div_term)
         pe_negative[:, 0::2] = torch.sin(-1 * position * div_term)
         pe_negative[:, 1::2] = torch.cos(-1 * position * div_term)
 
-        print("idk why it would fail here")
-
         # Reserve the order of positive indices and concat both positive and
         # negative indices. This is used to support the shifting trick
         # as in https://arxiv.org/abs/1901.02860
         pe_positive = torch.flip(pe_positive, [0]).unsqueeze(0)
-        print("or here")
 
         pe_negative = pe_negative[1:].unsqueeze(0)
-        print("also not here")
 
         pe = torch.cat([pe_positive, pe_negative], dim=1)
-        print("god I wish I could use a debugger for this")
 
-        self.pe = pe
+        self.pe = pe.to(dtype=x.dtype)
 
     def forward(self, x):
         """
@@ -141,12 +130,9 @@ class RelPositionalEncoding(torch.nn.Module):
         Returns:
             torch.Tensor: Encoded tensor (batch, time, `*`).
         """
-        print("AAAAAAAAAAAAAAAAAAAAAAAAAAA")
         self.extend_pe(x)
-        print("BBBBBBBBBBBBBBBBBBBBBBBBBBB")
         x = x * self.xscale
         pos_emb = self.pe[:, self.pe.size(1) // 2 - x.size(1) + 1: self.pe.size(1) // 2 + x.size(1), ]
-        print("whyyyy tho")
         return self.dropout(x), self.dropout(pos_emb)
 
 
