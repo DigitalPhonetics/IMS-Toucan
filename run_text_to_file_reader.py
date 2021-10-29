1718e6363097c10b857b15c94c8e7d54dbd943da
Flux9665 <florian.lux@ims.uni-stuttgart.de>
2021-10-29 13:45:09 +0200

    add infrastructure to create study

--- run_text_to_file_reader.py
+++ run_text_to_file_reader.py
@@ -69,12 +69,64 @@ if __name__ == '__main__':
     if not os.path.isdir("audios"):
         os.makedirs("audios")
 
-    read_texts(model_id="fast_nancy",
-               sentence=["Hello world, I am a synthesis voice."],
+    read_texts(model_id="fast_karlsson",
+               sentence=[
+                   "Einst stritten sich Nordwind und Sonne, wer von ihnen beiden wohl der Stärkere wäre, als ein Wanderer, der in einen warmen Mantel gehüllt war, des Weges daherkam."],
                device=exec_device,
-               filename="audios/fast_nancy.wav")
+               filename="audios/1_1.wav")
 
-    read_texts(model_id="taco_nancy",
-               sentence=["Hello world, I am a synthesis voice."],
+    read_texts(model_id="fast_karlsson",
+               sentence=["Sie wurden einig, dass derjenige für den Stärkeren gelten sollte, der den Wanderer zwingen würde, seinen Mantel abzunehmen."],
                device=exec_device,
-               filename="audios/fast_nancy.wav")
+               filename="audios/1_2.wav")
+
+    read_texts(model_id="fast_karlsson",
+               sentence=["Der Nordwind blies mit aller Macht, aber je mehr er blies, desto fester hüllte sich der Wanderer in seinen Mantel ein."],
+               device=exec_device,
+               filename="audios/1_3.wav")
+
+    read_texts(model_id="fast_karlsson",
+               sentence=["Endlich gab der Nordwind den Kampf auf."],
+               device=exec_device,
+               filename="audios/1_4.wav")
+
+    read_texts(model_id="fast_karlsson",
+               sentence=["Nun erwärmte die Sonne die Luft mit ihren freundlichen Strahlen, und schon nach wenigen Augenblicken zog der Wanderer seinen Mantel aus."],
+               device=exec_device,
+               filename="audios/1_5.wav")
+
+    read_texts(model_id="fast_karlsson",
+               sentence=["Da musste der Nordwind zugeben, dass die Sonne von ihnen beiden der Stärkere war."],
+               device=exec_device,
+               filename="audios/1_6.wav")
+
+    read_texts(model_id="fast_low",
+               sentence=[
+                   "Einst stritten sich Nordwind und Sonne, wer von ihnen beiden wohl der Stärkere wäre, als ein Wanderer, der in einen warmen Mantel gehüllt war, des Weges daherkam."],
+               device=exec_device,
+               filename="audios/2_1.wav")
+
+    read_texts(model_id="fast_low",
+               sentence=["Sie wurden einig, dass derjenige für den Stärkeren gelten sollte, der den Wanderer zwingen würde, seinen Mantel abzunehmen."],
+               device=exec_device,
+               filename="audios/2_2.wav")
+
+    read_texts(model_id="fast_low",
+               sentence=["Der Nordwind blies mit aller Macht, aber je mehr er blies, desto fester hüllte sich der Wanderer in seinen Mantel ein."],
+               device=exec_device,
+               filename="audios/2_3.wav")
+
+    read_texts(model_id="fast_low",
+               sentence=["Endlich gab der Nordwind den Kampf auf."],
+               device=exec_device,
+               filename="audios/2_4.wav")
+
+    read_texts(model_id="fast_low",
+               sentence=["Nun erwärmte die Sonne die Luft mit ihren freundlichen Strahlen, und schon nach wenigen Augenblicken zog der Wanderer seinen Mantel aus."],
+               device=exec_device,
+               filename="audios/2_5.wav")
+
+    read_texts(model_id="fast_low",
+               sentence=["Da musste der Nordwind zugeben, dass die Sonne von ihnen beiden der Stärkere war."],
+               device=exec_device,
+               filename="audios/2_6.wav")
