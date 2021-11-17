5c220906f58a81715af82dc0fa4c1b759ff27cf2
Flux9665 <florian.lux@ims.uni-stuttgart.de>
2021-11-17 21:28:03 +0100

    fix inference interfaces and test

--- run_text_to_file_reader.py
+++ run_text_to_file_reader.py
@@ -3,30 +3,12 @@ import os
 import torch
 
 from InferenceInterfaces.Eva_FastSpeech2 import Eva_FastSpeech2
-from InferenceInterfaces.Eva_Tacotron2 import Eva_Tacotron2
-from InferenceInterfaces.HokusPokus_FastSpeech2 import HokusPokus_FastSpeech2
-from InferenceInterfaces.HokusPokus_Tacotron2 import HokusPokus_Tacotron2
 from InferenceInterfaces.Karlsson_FastSpeech2 import Karlsson_FastSpeech2
-from InferenceInterfaces.Karlsson_Tacotron2 import Karlsson_Tacotron2
-from InferenceInterfaces.LowRes_FastSpeech2 import LowRes_FastSpeech2 as fast_low
-from InferenceInterfaces.LowRes_Tacotron2 import LowRes_Tacotron2 as taco_low
 from InferenceInterfaces.Nancy_FastSpeech2 import Nancy_FastSpeech2
-from InferenceInterfaces.Nancy_Tacotron2 import Nancy_Tacotron2
 
 tts_dict = {
     "fast_nancy"   : Nancy_FastSpeech2,
-    "fast_hokus"   : HokusPokus_FastSpeech2,
-
-    "taco_nancy"   : Nancy_Tacotron2,
-    "taco_hokus"   : HokusPokus_Tacotron2,
-
-    "taco_low"     : taco_low,
-    "fast_low"     : fast_low,
-
-    "taco_eva"     : Eva_Tacotron2,
     "fast_eva"     : Eva_FastSpeech2,
-
-    "taco_karlsson": Karlsson_Tacotron2,
     "fast_karlsson": Karlsson_FastSpeech2,
     }
 
@@ -73,62 +55,9 @@ if __name__ == '__main__':
                sentence=[
                    "Einst stritten sich Nordwind und Sonne, wer von ihnen beiden wohl der Stärkere wäre, als ein Wanderer, der in einen warmen Mantel gehüllt war, des Weges daherkam."],
                device=exec_device,
-               filename="audios/1_1.wav")
+               filename="audios/1.wav")
 
-    read_texts(model_id="fast_karlsson",
-               sentence=["Sie wurden einig, dass derjenige für den Stärkeren gelten sollte, der den Wanderer zwingen würde, seinen Mantel abzunehmen."],
+    read_texts(model_id="fast_nancy",
+               sentence=["This is an English sentence, I really hope that it works."],
                device=exec_device,
-               filename="audios/1_2.wav")
-
-    read_texts(model_id="fast_karlsson",
-               sentence=["Der Nordwind blies mit aller Macht, aber je mehr er blies, desto fester hüllte sich der Wanderer in seinen Mantel ein."],
-               device=exec_device,
-               filename="audios/1_3.wav")
-
-    read_texts(model_id="fast_karlsson",
-               sentence=["Endlich gab der Nordwind den Kampf auf."],
-               device=exec_device,
-               filename="audios/1_4.wav")
-
-    read_texts(model_id="fast_karlsson",
-               sentence=[
-                   "Nun erwärmte die Sonne die Luft mit ihren freundlichen Strahlen, und schon nach wenigen Augenblicken zog der Wanderer seinen Mantel aus."],
-               device=exec_device,
-               filename="audios/1_5.wav")
-
-    read_texts(model_id="fast_karlsson",
-               sentence=["Da musste der Nordwind zugeben, dass die Sonne von ihnen beiden der Stärkere war."],
-               device=exec_device,
-               filename="audios/1_6.wav")
-
-    read_texts(model_id="fast_low",
-               sentence=[
-                   "Einst stritten sich Nordwind und Sonne, wer von ihnen beiden wohl der Stärkere wäre, als ein Wanderer, der in einen warmen Mantel gehüllt war, des Weges daherkam."],
-               device=exec_device,
-               filename="audios/2_1.wav")
-
-    read_texts(model_id="fast_low",
-               sentence=["Sie wurden einig, dass derjenige für den Stärkeren gelten sollte, der den Wanderer zwingen würde, seinen Mantel abzunehmen."],
-               device=exec_device,
-               filename="audios/2_2.wav")
-
-    read_texts(model_id="fast_low",
-               sentence=["Der Nordwind blies mit aller Macht, aber je mehr er blies, desto fester hüllte sich der Wanderer in seinen Mantel ein."],
-               device=exec_device,
-               filename="audios/2_3.wav")
-
-    read_texts(model_id="fast_low",
-               sentence=["Endlich gab der Nordwind den Kampf auf."],
-               device=exec_device,
-               filename="audios/2_4.wav")
-
-    read_texts(model_id="fast_low",
-               sentence=[
-                   "Nun erwärmte die Sonne die Luft mit ihren freundlichen Strahlen, und schon nach wenigen Augenblicken zog der Wanderer seinen Mantel aus."],
-               device=exec_device,
-               filename="audios/2_5.wav")
-
-    read_texts(model_id="fast_low",
-               sentence=["Da musste der Nordwind zugeben, dass die Sonne von ihnen beiden der Stärkere war."],
-               device=exec_device,
-               filename="audios/2_6.wav")
+               filename="audios/2.wav")
--- run_weight_averaging.py
+++ run_weight_averaging.py
@@ -7,15 +7,7 @@ import os
 import torch
 
 from TrainingInterfaces.Spectrogram_to_Wave.HiFIGAN.HiFiGAN import HiFiGANGenerator
-from TrainingInterfaces.Text_to_Spectrogram.AutoAligner.Aligner import Tacotron2
 from TrainingInterfaces.Text_to_Spectrogram.FastSpeech2.FastSpeech2 import FastSpeech2
-
-
-def load_net_taco(path):
-    check_dict = torch.load(path, map_location=torch.device("cpu"))
-    net = Tacotron2()
-    net.load_state_dict(check_dict["model"])
-    return net
 
 
 def load_net_fast(path):
@@ -88,10 +80,6 @@ def make_best_in_all(n=3):
             checkpoint_paths = get_n_recent_checkpoints_paths(checkpoint_dir="Models/{}".format(model_dir), n=n)
             averaged_model = average_checkpoints(checkpoint_paths, load_func=load_net_hifigan)
             save_model_for_use(model=averaged_model, name="Models/{}/best.pt".format(model_dir), dict_name="generator")
-        elif "Tacotron2" in model_dir:
-            checkpoint_paths = get_n_recent_checkpoints_paths(checkpoint_dir="Models/{}".format(model_dir), n=n)
-            averaged_model = average_checkpoints(checkpoint_paths, load_func=load_net_taco)
-            save_model_for_use(model=averaged_model, name="Models/{}/best.pt".format(model_dir))
         elif "FastSpeech2" in model_dir:
             checkpoint_paths = get_n_recent_checkpoints_paths(checkpoint_dir="Models/{}".format(model_dir), n=n)
             averaged_model = average_checkpoints(checkpoint_paths, load_func=load_net_fast)
@@ -103,9 +91,7 @@ def count_parameters(net):
 
 
 def show_all_models_params():
-    from TrainingInterfaces.Text_to_Spectrogram.AutoAligner.Aligner import Tacotron2
     from TrainingInterfaces.Text_to_Spectrogram.FastSpeech2.FastSpeech2 import FastSpeech2
-    print("Number of (trainable) Parameters in Tacotron2: {}".format(count_parameters(Tacotron2())))
     print("Number of (trainable) Parameters in FastSpeech2: {}".format(count_parameters(FastSpeech2())))
 
 
