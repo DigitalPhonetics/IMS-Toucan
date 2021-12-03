fix broadcasting of shapes

TrainingInterfaces/Text_to_Spectrogram/FastSpeech2/EnergyCalculator.py
@@ -67,7 +67,7 @@ class EnergyCalculator(torch.nn.Module):
 
         # Return with the shape (B, T, 1)
         if norm_by_average:
-            average = energy[energy != 0.0].mean()
+            average = energy[0][energy[0] != 0.0].mean()
             energy = energy / average
         return energy.unsqueeze(-1), energy_lengths
 

TrainingInterfaces/Text_to_Spectrogram/FastSpeech2/PitchCalculator.py
@@ -67,7 +67,7 @@ class Dio(torch.nn.Module):
 
         # Return with the shape (B, T, 1)
         if norm_by_average:
-            average = pitch[pitch != 0.0].mean()
+            average = pitch[0][pitch[0] != 0.0].mean()
             pitch = pitch / average
         return pitch.unsqueeze(-1), pitch_lengths
 