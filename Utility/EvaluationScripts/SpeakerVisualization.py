ffcb56b52863fd4325d19860f43ecbf3b5e51fcd
Florian Lux <florian.lux@ims.uni-stuttgart.de>
2022-02-22 21:49:27 +0100

    improve speaker embedding visualization

--- Utility/EvaluationScripts/SpeakerVisualization.py
+++ Utility/EvaluationScripts/SpeakerVisualization.py
@@ -23,31 +23,35 @@ class Visualizer:
         label_list = list()
         embedding_list = list()
         for label in tqdm(label_to_filepaths):
-            label_list.append(label)
-            wave, sr = sf.read(label_to_filepaths[label])
-            if self.sr != sr:
-                print("One of the Audios you included doesn't match the sampling rate of this visualizer object, "
-                      "creating a new condition extractor. Results will be correct, but if there are too many cases "
-                      "of changing samplingrate, this will run very slowly.")
-                self.pros_cond_ext = ProsodicConditionExtractor(sr=sr)
-                self.sr = sr
-            embedding_list.append(self.pros_cond_ext.extract_condition_from_reference_wave(wave).squeeze().numpy())
+            for filepath in tqdm(label_to_filepaths[label]):
+                wave, sr = sf.read(filepath)
+                if len(wave) / sr < 1:
+                    continue
+                if self.sr != sr:
+                    print("One of the Audios you included doesn't match the sampling rate of this visualizer object, "
+                          "creating a new condition extractor. Results will be correct, but if there are too many cases "
+                          "of changing samplingrate, this will run very slowly.")
+                    self.pros_cond_ext = ProsodicConditionExtractor(sr=sr)
+                    self.sr = sr
+                embedding_list.append(self.pros_cond_ext.extract_condition_from_reference_wave(wave).squeeze().numpy())
+                label_list.append(label)
         embeddings_as_array = numpy.array(embedding_list)
         dimensionality_reduced_embeddings = self.tsne.fit_transform(embeddings_as_array)
         self._plot_embeddings(dimensionality_reduced_embeddings, label_list, title=title_of_plot)
 
     def _plot_embeddings(self, projected_data, labels, title):
         label_to_color = dict()
-        for label in set(labels):
-            label_to_color[label] = numpy.random.random()
+        for index, label in enumerate(list(set(labels))):
+            label_to_color[label] = (1 / len(label_to_color)) * index
         plt.clf()
         plt.scatter(x=[x[0] for x in projected_data],
                     y=[x[1] for x in projected_data],
                     marker=MarkerStyle(marker="."),
                     c=[label_to_color[label] for label in labels],
-                    cmap='gist_rainbow')
+                    cmap='gist_rainbow',
+                    alpha=0.6)
         plt.tight_layout()
         plt.axis('off')
-        plt.subplots_adjust(top=0.85)
+        plt.subplots_adjust(top=0.9, bottom=0.0, right=1.0, left=0.0)
         plt.title(title)
         plt.show()
--- run_speaker_visualization.py
+++ run_speaker_visualization.py
@@ -3,8 +3,8 @@ import os
 from Utility.EvaluationScripts.SpeakerVisualization import Visualizer
 from run_text_to_file_reader import read_texts_as_ensemble
 
-if __name__ == '__main__':
 
+def visualize_random_speakers():
     # first we generate a bunch of audios with random speaker embeddings
     if not len(os.listdir("audios/random_speakers/")) != 0:
         os.makedirs("audios/random_speakers/", exist_ok=True)
@@ -15,5 +15,22 @@ if __name__ == '__main__':
     vs = Visualizer()
     ltf = dict()
     for audio_file in os.listdir("audios/random_speakers"):
-        ltf[audio_file] = f"audios/random_speakers/{audio_file}"
+        if audio_file not in ltf:
+            ltf[audio_file] = list()
+        ltf[audio_file].append(f"audios/random_speakers/{audio_file}")
     vs.visualize_speaker_embeddings(label_to_filepaths=ltf, title_of_plot="Embeddings of TTS with random Condition")
+
+
+def visualize_libritts():
+    vs = Visualizer()
+    ltf = dict()
+    for speaker in os.listdir("audios/LibriTTS"):
+        ltf[speaker] = list()
+        for book in os.listdir(f"audios/LibriTTS/{speaker}"):
+            for audio_file in os.listdir(f"audios/LibriTTS/{speaker}/{book}"):
+                ltf[speaker].append(f"audios/LibriTTS/{speaker}/{book}/{audio_file}")
+    vs.visualize_speaker_embeddings(label_to_filepaths=ltf, title_of_plot="Embeddings of a Subset of LibriTTS")
+
+
+if __name__ == '__main__':
+    visualize_libritts()
