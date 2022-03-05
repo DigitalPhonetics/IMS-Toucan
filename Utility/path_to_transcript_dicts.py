65fa6422c16a02cd6f84c4a11d31a321b6bf5e0b
Florian Lux <florian.lux@ims.uni-stuttgart.de>
2022-03-05 20:39:36 +0100

    add another dataset to the meta pipeline

--- Utility/path_to_transcript_dicts.py
+++ Utility/path_to_transcript_dicts.py
@@ -189,6 +189,20 @@ def build_path_to_transcript_dict_libritts():
     return path_to_transcript
 
 
+def build_path_to_transcript_dict_libritts_all_clean():
+    path_train = "/mount/resources/speech/corpora/LibriTTS/all-clean"
+    path_to_transcript = dict()
+    for speaker in os.listdir(path_train):
+        for chapter in os.listdir(os.path.join(path_train, speaker)):
+            for file in os.listdir(os.path.join(path_train, speaker, chapter)):
+                if file.endswith("normalized.txt"):
+                    with open(os.path.join(path_train, speaker, chapter, file), 'r', encoding='utf8') as tf:
+                        transcript = tf.read()
+                    wav_file = file.split(".")[0] + ".wav"
+                    path_to_transcript[os.path.join(path_train, speaker, chapter, wav_file)] = transcript
+    return path_to_transcript
+
+
 def build_path_to_transcript_dict_libritts_other500():
     path_train = "/mount/resources/asr-data/LibriTTS/train-other-500"
     path_to_transcript = dict()
