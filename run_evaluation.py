f880109b3a5e38915fe110840bca43996dbeed41
Florian Lux <florian.lux@ims.uni-stuttgart.de>
2022-03-14 17:43:51 +0100

    use torch to speed up dtw computation

--- run_evaluation.py
+++ run_evaluation.py
@@ -3,7 +3,7 @@ import os
 from tqdm import tqdm
 
 from Utility.EvaluationScripts.audio_vs_audio import ffe
-from Utility.EvaluationScripts.audio_vs_audio import mcd_with_warping
+from Utility.EvaluationScripts.audio_vs_audio import soft_mcd
 
 mcd_lyric_cloned = list()
 mcd_lyric_uncond = list()
@@ -17,14 +17,14 @@ ffe_prose_uncond = list()
 
 for file in tqdm(os.listdir("audios/evaluation/human")):
     if file.endswith(".wav"):
-        mcd_lyric_cloned.append(mcd_with_warping(f"audios/evaluation/human/{file}",
-                                                 f"audios/evaluation/poetry_cloned/{file.split('.')[0]}_poetic_cloned.wav"))
-        mcd_lyric_uncond.append(mcd_with_warping(f"audios/evaluation/human/{file}",
-                                                 f"audios/evaluation/poetry_unconditional/{file.split('.')[0]}_poetic_uncond.wav"))
-        mcd_prose_cloned.append(mcd_with_warping(f"audios/evaluation/human/{file}",
-                                                 f"audios/evaluation/prosa_cloned/{file.split('.')[0]}_prosa_cloned.wav"))
-        mcd_prose_uncond.append(mcd_with_warping(f"audios/evaluation/human/{file}",
-                                                 f"audios/evaluation/prosa_unconditional/{file.split('.')[0]}_prosa_uncond.wav"))
+        mcd_lyric_cloned.append(soft_mcd(f"audios/evaluation/human/{file}",
+                                         f"audios/evaluation/poetry_cloned/{file.split('.')[0]}_poetic_cloned.wav"))
+        mcd_lyric_uncond.append(soft_mcd(f"audios/evaluation/human/{file}",
+                                         f"audios/evaluation/poetry_unconditional/{file.split('.')[0]}_poetic_uncond.wav"))
+        mcd_prose_cloned.append(soft_mcd(f"audios/evaluation/human/{file}",
+                                         f"audios/evaluation/prosa_cloned/{file.split('.')[0]}_prosa_cloned.wav"))
+        mcd_prose_uncond.append(soft_mcd(f"audios/evaluation/human/{file}",
+                                         f"audios/evaluation/prosa_unconditional/{file.split('.')[0]}_prosa_uncond.wav"))
 
         ffe_lyric_cloned.append(ffe(f"audios/evaluation/human/{file}", f"audios/evaluation/poetry_cloned/{file.split('.')[0]}_poetic_cloned.wav"))
         ffe_lyric_uncond.append(ffe(f"audios/evaluation/human/{file}", f"audios/evaluation/poetry_unconditional/{file.split('.')[0]}_poetic_uncond.wav"))
