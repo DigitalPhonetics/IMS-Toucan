601aed23fd96b549d1f2402d0b29c82e29202a21
Florian Lux <florian.lux@ims.uni-stuttgart.de>
2022-03-14 15:13:38 +0100

    run evaluation on poetry data

--- run_evaluation.py
+++ run_evaluation.py
@@ -3,67 +3,40 @@ import os
 from tqdm import tqdm
 
 from Utility.EvaluationScripts.audio_vs_audio import ffe
-# from Utility.EvaluationScripts.playground_audio_vs_audio_poem import get_pitch_curves_abc
-from Utility.EvaluationScripts.audio_vs_audio import gpe
 from Utility.EvaluationScripts.audio_vs_audio import mcd_with_warping
-from Utility.EvaluationScripts.audio_vs_audio import vde
 
-# get_pitch_curves_abc(f"audios/ps2/PoetryStudy/Set5/s5_p1_ref2.wav", f"audios/ps2/PoetryStudy/Set5/s5_p1_ref1.wav", f"audios/ps2/PoetryStudy/Set5/s5_p1_base2_pros_1.wav")
+mcd_lyric_cloned = list()
+mcd_lyric_uncond = list()
+mcd_prose_cloned = list()
+mcd_prose_uncond = list()
 
-mcd_same_style = list()
-mcd_diff_style = list()
+ffe_lyric_cloned = list()
+ffe_lyric_uncond = list()
+ffe_prose_cloned = list()
+ffe_prose_uncond = list()
 
-ffe_same_style = list()
-ffe_diff_style = list()
-
-gpe_same_style = list()
-gpe_diff_style = list()
-
-vde_same_style = list()
-vde_diff_style = list()
-
-for file in tqdm(os.listdir("audios/adept/human")):
+for file in tqdm(os.listdir("audios/evaluation/human")):
     if file.endswith(".wav"):
-        mcd_same_style.append(mcd_with_warping(f"audios/adept/human/{file}", f"audios/adept/same_voice_same_style/{file}"))
-        vde_same_style.append(vde(f"audios/adept/human/{file}", f"audios/adept/same_voice_same_style/{file}"))
-        gpe_same_style.append(gpe(f"audios/adept/human/{file}", f"audios/adept/same_voice_same_style/{file}"))
-        ffe_same_style.append(ffe(f"audios/adept/human/{file}", f"audios/adept/same_voice_same_style/{file}"))
+        mcd_lyric_cloned.append(mcd_with_warping(f"audios/evaluation/human/{file}",
+                                                 f"audios/evaluation/poetry_cloned/{file.split('.')[0]}_poetic_cloned.wav"))
+        mcd_lyric_uncond.append(mcd_with_warping(f"audios/evaluation/human/{file}",
+                                                 f"audios/evaluation/poetry_unconditional/{file.split('.')[0]}_poetic_uncond.wav"))
+        mcd_prose_cloned.append(mcd_with_warping(f"audios/evaluation/human/{file}",
+                                                 f"audios/evaluation/prosa_cloned/{file.split('.')[0]}_prosa_cloned.wav"))
+        mcd_prose_uncond.append(mcd_with_warping(f"audios/evaluation/human/{file}",
+                                                 f"audios/evaluation/prosa_unconditional/{file.split('.')[0]}_prosa_uncond.wav"))
 
-        mcd_diff_style.append(mcd_with_warping(f"audios/adept/human/{file}", f"audios/adept/same_voice_diff_style/{file}"))
-        vde_diff_style.append(vde(f"audios/adept/human/{file}", f"audios/adept/same_voice_diff_style/{file}"))
-        gpe_diff_style.append(gpe(f"audios/adept/human/{file}", f"audios/adept/same_voice_diff_style/{file}"))
-        ffe_diff_style.append(ffe(f"audios/adept/human/{file}", f"audios/adept/same_voice_diff_style/{file}"))
+        ffe_lyric_cloned.append(ffe(f"audios/evaluation/human/{file}", f"audios/evaluation/poetry_cloned/{file.split('.')[0]}_poetic_cloned.wav"))
+        ffe_lyric_uncond.append(ffe(f"audios/evaluation/human/{file}", f"audios/evaluation/poetry_unconditional/{file.split('.')[0]}_poetic_uncond.wav"))
+        ffe_prose_cloned.append(ffe(f"audios/evaluation/human/{file}", f"audios/evaluation/prosa_cloned/{file.split('.')[0]}_prosa_cloned.wav"))
+        ffe_prose_uncond.append(ffe(f"audios/evaluation/human/{file}", f"audios/evaluation/prosa_unconditional/{file.split('.')[0]}_prosa_uncond.wav"))
 
-print(mcd_same_style)
-print(vde_same_style)
-print(gpe_same_style)
-print(ffe_same_style)
+print((1 / len(mcd_lyric_cloned)) * sum(mcd_lyric_cloned))
+print((1 / len(mcd_lyric_uncond)) * sum(mcd_lyric_uncond))
+print((1 / len(mcd_prose_cloned)) * sum(mcd_prose_cloned))
+print((1 / len(mcd_prose_uncond)) * sum(mcd_prose_uncond))
 
-print(mcd_diff_style)
-print(vde_diff_style)
-print(gpe_diff_style)
-print(ffe_diff_style)
-
-print((1 / len(mcd_same_style)) * sum(mcd_same_style))
-print((1 / len(vde_same_style)) * sum(vde_same_style))
-print((1 / len(gpe_same_style)) * sum(gpe_same_style))
-print((1 / len(ffe_same_style)) * sum(ffe_same_style))
-
-print((1 / len(mcd_diff_style)) * sum(mcd_diff_style))
-print((1 / len(vde_diff_style)) * sum(vde_diff_style))
-print((1 / len(gpe_diff_style)) * sum(gpe_diff_style))
-print((1 / len(ffe_diff_style)) * sum(ffe_diff_style))
-
-"""
-Results on ADEPT
-
-25.628487893016743
-0.3197801087172897
-0.6701621670474431
-0.6701621670474431
-
-5.585193124795971
-0.4236358253409133
-0.7842741112107358
-0.7842741112107358
-"""
+print((1 / len(ffe_lyric_cloned)) * sum(ffe_lyric_cloned))
+print((1 / len(ffe_lyric_uncond)) * sum(ffe_lyric_uncond))
+print((1 / len(ffe_prose_cloned)) * sum(ffe_prose_cloned))
+print((1 / len(ffe_prose_uncond)) * sum(ffe_prose_uncond))
