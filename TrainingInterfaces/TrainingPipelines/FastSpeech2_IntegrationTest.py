5e86d74d4fff926b7c6c27e504d997ff1604f7a7
Florian Lux <florian.lux@ims.uni-stuttgart.de>
2022-06-13 01:30:44 +0200

    mode langs in integration test

--- TrainingInterfaces/TrainingPipelines/FastSpeech2_IntegrationTest.py
+++ TrainingInterfaces/TrainingPipelines/FastSpeech2_IntegrationTest.py
@@ -39,7 +39,7 @@ def run(gpu_id, resume_checkpoint, finetune, model_dir, resume):
                                           lang="en",
                                           save_imgs=True)
 
-    model = FastSpeech2(lang_embs=10, utt_embed_dim=128)
+    model = FastSpeech2(lang_embs=100, utt_embed_dim=128)
 
     print("Training model")
     train_loop(net=model,
