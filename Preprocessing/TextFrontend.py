330f347337439a12412f5edbf970d2ed58bd0e78
Florian Lux <florian.lux@ims.uni-stuttgart.de>
2022-08-26 15:41:19 +0200

    fix some incorrect indexes once more

--- Preprocessing/TextFrontend.py
+++ Preprocessing/TextFrontend.py
@@ -191,13 +191,13 @@ class ArticulatoryCombinedTextFrontend:
             # affects previous phoneme -----------------
             elif char == '\u02D0':
                 # lengthened
-                phones_vector[-1][8] = 1
+                phones_vector[-1][10] = 1
             elif char == '\u02D1':
                 # half length
-                phones_vector[-1][9] = 1
+                phones_vector[-1][11] = 1
             elif char == '\u0306':
                 # shortened
-                phones_vector[-1][10] = 1
+                phones_vector[-1][12] = 1
             elif char == "˥":
                 # very high tone
                 phones_vector[-1][1] = 1
@@ -221,10 +221,10 @@ class ArticulatoryCombinedTextFrontend:
                 phones_vector[-1][7] = 1
             elif char == "⮁":
                 # peaking tone
-                phones_vector[-1][60] = 1
+                phones_vector[-1][8] = 1
             elif char == "⮃":
                 # dipping tone
-                phones_vector[-1][61] = 1
+                phones_vector[-1][9] = 1
             else:
                 if handle_missing:
                     try:
