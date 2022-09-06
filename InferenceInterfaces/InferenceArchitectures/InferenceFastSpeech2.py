fix inference bug and implement pause scaling

InferenceInterfaces/InferenceArchitectures/InferenceFastSpeech2.py
@@ -194,9 +194,8 @@ class FastSpeech2(torch.nn.Module, ABC):
             duration_predictions = self.duration_predictor(encoded_texts, duration_masks)
 
             for phoneme_index, phoneme_vector in enumerate(text_tensors):
-                if phoneme_vector[59] == 0:
+                if phoneme_vector[61] == 0:
                     pitch_predictions[phoneme_index] = 0.0
-                    energy_predictions[phoneme_index] = 0.0
                 if phoneme_vector[16] == 1 and pause_duration_scaling_factor != 1.0:
                     duration_predictions[0][phoneme_index] = torch.round(duration_predictions[0][phoneme_index].float() * pause_duration_scaling_factor)
             pitch_predictions = _scale_variance(pitch_predictions, pitch_variance_scale)