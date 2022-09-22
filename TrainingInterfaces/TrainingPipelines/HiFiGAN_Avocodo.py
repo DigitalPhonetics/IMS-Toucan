fe272e790bc6b0f53ee759019ac45f1d9586e44e
Florian Lux <florian.lux@ims.uni-stuttgart.de>
2022-09-22 03:38:53 +0200

    minor fix

--- TrainingInterfaces/TrainingPipelines/HiFiGAN_Avocodo.py
+++ TrainingInterfaces/TrainingPipelines/HiFiGAN_Avocodo.py
@@ -31,48 +31,52 @@ def run(gpu_id, resume_checkpoint, finetune, resume, model_dir, use_wandb):
         model_save_dir = model_dir
     else:
         model_save_dir = "Models/HiFiGAN_Avocodo"
-    if not os.path.exists(model_save_dir):
-        os.makedirs(model_save_dir)
+    os.makedirs(model_save_dir, exist_ok=True)
+
+    full_lists_to_sample_sizes = {
+        tuple(build_path_to_transcript_dict_mls_italian().keys())           : 1500,
+        tuple(build_path_to_transcript_dict_mls_french().keys())            : 1500,
+        tuple(build_path_to_transcript_dict_mls_dutch().keys())             : 1500,
+        tuple(build_path_to_transcript_dict_mls_polish().keys())            : 1500,
+        tuple(build_path_to_transcript_dict_mls_spanish().keys())           : 1500,
+        tuple(build_path_to_transcript_dict_mls_portuguese().keys())        : 1500,
+        tuple(build_path_to_transcript_dict_karlsson().keys())              : 500,
+        tuple(build_path_to_transcript_dict_eva().keys())                   : 500,
+        tuple(build_path_to_transcript_dict_bernd().keys())                 : 500,
+        tuple(build_path_to_transcript_dict_friedrich().keys())             : 500,
+        tuple(build_path_to_transcript_dict_hokus().keys())                 : 500,
+        tuple(build_path_to_transcript_dict_hui_others().keys())            : 1500,
+        tuple(build_path_to_transcript_dict_elizabeth().keys())             : 500,
+        tuple(build_path_to_transcript_dict_nancy().keys())                 : 500,
+        tuple(build_path_to_transcript_dict_hokuspokus().keys())            : 500,
+        tuple(build_path_to_transcript_dict_fluxsing().keys())              : 500,
+        tuple(build_path_to_transcript_dict_vctk().keys())                  : 500,
+        tuple(build_path_to_transcript_dict_libritts_all_clean().keys())    : 3000,
+        tuple(build_path_to_transcript_dict_ljspeech().keys())              : 500,
+        tuple(build_path_to_transcript_dict_css10cmn().keys())              : 500,
+        tuple(build_path_to_transcript_dict_vietTTS().keys())               : 500,
+        tuple(build_path_to_transcript_dict_thorsten().keys())              : 500,
+        tuple(build_path_to_transcript_dict_css10el().keys())               : 500,
+        tuple(build_path_to_transcript_dict_css10nl().keys())               : 500,
+        tuple(build_path_to_transcript_dict_css10fi().keys())               : 500,
+        tuple(build_path_to_transcript_dict_css10ru().keys())               : 500,
+        tuple(build_path_to_transcript_dict_css10hu().keys())               : 500,
+        tuple(build_path_to_transcript_dict_css10es().keys())               : 500,
+        tuple(build_path_to_transcript_dict_css10fr().keys())               : 500,
+        tuple(build_path_to_transcript_dict_nvidia_hifitts().keys())        : 500,
+        tuple(build_path_to_transcript_dict_spanish_blizzard_train().keys()): 500,
+        tuple(build_path_to_transcript_dict_aishell3().keys())              : 1500,
+        tuple(build_path_to_transcript_dict_VIVOS_viet().keys())            : 500,
+        tuple(build_path_to_transcript_dict_RAVDESS().keys())               : 500,
+        tuple(build_path_to_transcript_dict_ESDS().keys())                  : 1500,
+        }
 
     # sampling multiple times from the dataset, because it's too big to fit all at once
     for run_id in range(1000):
-
+        print("Preparing new data...")
         file_lists = list()
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_mls_italian().keys()), 1500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_mls_french().keys()), 1500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_mls_dutch().keys()), 1500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_mls_polish().keys()), 1500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_mls_spanish().keys()), 1500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_mls_portuguese().keys()), 1500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_karlsson().keys()), 500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_eva().keys()), 500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_bernd().keys()), 500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_friedrich().keys()), 500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_hokus().keys()), 500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_hui_others().keys()), 1500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_elizabeth().keys()), 500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_nancy().keys()), 500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_hokuspokus().keys()), 500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_fluxsing().keys()), 500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_vctk().keys()), 500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_libritts_all_clean().keys()), 3000))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_ljspeech().keys()), 500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_css10cmn().keys()), 500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_vietTTS().keys()), 500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_thorsten().keys()), 500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_css10el().keys()), 500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_css10nl().keys()), 500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_css10fi().keys()), 500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_css10ru().keys()), 500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_css10hu().keys()), 500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_css10es().keys()), 500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_css10fr().keys()), 500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_nvidia_hifitts().keys()), 500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_spanish_blizzard_train().keys()), 500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_aishell3().keys()), 1500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_VIVOS_viet().keys()), 500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_RAVDESS().keys()), 500))
-        file_lists.append(random.sample(list(build_path_to_transcript_dict_ESDS().keys()), 1500))
+        for file_list in full_lists_to_sample_sizes:
+            file_lists.append(random.sample(file_list, full_lists_to_sample_sizes[file_list]))
 
         datasets = list()
 
