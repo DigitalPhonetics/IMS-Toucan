4af4c7c04dc5bffae225dc507727261f702738fb
Flux9665 <33219702+Flux9665@users.noreply.github.com>
2022-10-19 01:53:02 +0200

    Update run_training_pipeline.py

--- run_training_pipeline.py
+++ run_training_pipeline.py
@@ -4,7 +4,7 @@ import sys
 from TrainingInterfaces.TrainingPipelines.FastSpeech2_IntegrationTest import run as integration_test
 from TrainingInterfaces.TrainingPipelines.FastSpeech2_IntegrationTestVietnamese import run as integration_test_vietnamese
 from TrainingInterfaces.TrainingPipelines.FastSpeech2_MetaCheckpoint import run as meta_fast
-from TrainingInterfaces.TrainingPipelines.FastSpeech2_finetune_to_German import run as fine_ger
+from TrainingInterfaces.TrainingPipelines.FastSpeech2_finetuning_example import run as fine_ger
 from TrainingInterfaces.TrainingPipelines.HiFiGAN_combined import run as hifigan_combined
 from TrainingInterfaces.TrainingPipelines.pretrain_aligner import run as aligner
 
