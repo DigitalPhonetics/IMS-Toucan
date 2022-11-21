1a691db6a63a3c8ecf4096b472fce6236b6a8d6c
CaraDuf <91517923+Ca-ressemble-a-du-fake@users.noreply.github.com>
2022-11-21 06:19:05 +0100

    Sped-up scorer by using GPU when available

--- run_scorer.py
+++ run_scorer.py
@@ -6,12 +6,15 @@ Example use of the scorer utility to inspect data.
 
 from Utility.Scorer import AlignmentScorer
 from Utility.Scorer import TTSScorer
+import torch
 
-alignment_scorer = AlignmentScorer(path_to_aligner_model="Models/Aligner/aligner.pt", device="cpu")
+exec_device = "cuda" if torch.cuda.is_available() else "cpu"
+
+alignment_scorer = AlignmentScorer(path_to_aligner_model="Models/Aligner/aligner.pt", device=exec_device)
 alignment_scorer.score(path_to_aligner_dataset="Corpora/IntegrationTest/aligner_train_cache.pt")
 alignment_scorer.show_samples_with_highest_loss(20)
 
-tts_scorer = TTSScorer(path_to_fastspeech_model="Models/FastSpeech2_IntegrationTest/best.pt", device="cpu")
+tts_scorer = TTSScorer(path_to_fastspeech_model="Models/FastSpeech2_IntegrationTest/best.pt", device=exec_device)
 tts_scorer.score(path_to_fastspeech_dataset="Corpora/IntegrationTest/", lang_id="en")
 tts_scorer.show_samples_with_highest_loss(20)
 tts_scorer.remove_samples_with_highest_loss(20)
