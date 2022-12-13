5db757a45679a95b15ab7f585debf863d2b46f1d
Florian Lux <lux.florian@gmail.com>
2022-12-13 16:42:38 +0100

    make padding consistent between VAE and Conformer

--- TrainingInterfaces/Text_to_Spectrogram/PortaSpeech/PortaSpeech.py
+++ TrainingInterfaces/Text_to_Spectrogram/PortaSpeech/PortaSpeech.py
@@ -356,9 +356,10 @@ class PortaSpeech(torch.nn.Module, ABC):
                                                cond=encoded_texts.transpose(1, 2)).transpose(1, 2)
         else:
             # forward fastspeech decoder instead, because the VAE likes to turn to NaNs if left on its own
-            gold_speech = cut_to_multiple_of_n(gold_speech)
-            encoded_texts = cut_to_multiple_of_n(encoded_texts)
-            speech_lens[speech_lens > gold_speech.size(1)] = gold_speech.size(1)
+            if not is_inference:
+                gold_speech = cut_to_multiple_of_n(gold_speech)
+                encoded_texts = cut_to_multiple_of_n(encoded_texts)
+                speech_lens[speech_lens > gold_speech.size(1)] = gold_speech.size(1)
 
             if speech_lens is not None and not is_inference:
                 h_masks = self._source_mask(speech_lens)
