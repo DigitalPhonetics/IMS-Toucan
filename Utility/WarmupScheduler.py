603aad5e4f2d84c8026f08fd7c41342d21f44e39
flux9665 <lux.florian@gmail.com>
2023-02-02 22:41:08 +0100

    re-add old scheduler for legacy support

--- Utility/WarmupScheduler.py
+++ Utility/WarmupScheduler.py
@@ -1,7 +1,3 @@
-"""
-Taken from ESPNet
-"""
-
 from torch.optim.lr_scheduler import _LRScheduler
 
 
@@ -32,3 +28,33 @@ class ToucanWarmupScheduler(_LRScheduler):
         else:
             scale = 1 - (((step_num - self.warmup_steps) / self.max_steps) / (self.max_steps / 10))
             return [max(lr * scale, 1e-7) for lr in self.base_lrs]
+
+
+class WarmupScheduler(_LRScheduler):
+    """
+    The WarmupLR scheduler
+    This scheduler is almost same as NoamLR Scheduler except for following difference:
+    NoamLR:
+        lr = optimizer.lr * model_size ** -0.5
+             * min(step ** -0.5, step * warmup_step ** -1.5)
+    WarmupLR:
+        lr = optimizer.lr * warmup_step ** 0.5
+             * min(step ** -0.5, step * warmup_step ** -1.5)
+    Note that the maximum lr equals to optimizer.lr in this scheduler.
+
+    Taken from ESPnet
+    """
+
+    def __init__(self, optimizer, warmup_steps=25000, last_epoch=-1):
+        self.warmup_steps = warmup_steps
+        # __init__() must be invoked before setting field
+        # because step() is also invoked in __init__()
+        super().__init__(optimizer, last_epoch)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(warmup_steps={self.warmup_steps})"
+
+    def get_lr(self):
+        step_num = self.last_epoch + 1
+        return [lr * self.warmup_steps ** 0.5 * min(step_num ** -0.5, step_num * self.warmup_steps ** -1.5) for lr in
+                self.base_lrs]
