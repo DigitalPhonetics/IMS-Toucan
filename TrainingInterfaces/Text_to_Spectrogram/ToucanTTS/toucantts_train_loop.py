f4c13b3d2a46524dd144d6521218627aaa9ea94f
Florian Lux <lux.florian@gmail.com>
2023-03-02 18:04:10 +0100

    further attempts at stabilizing the discriminator

--- TrainingInterfaces/Text_to_Spectrogram/ToucanTTS/toucantts_train_loop.py
+++ TrainingInterfaces/Text_to_Spectrogram/ToucanTTS/toucantts_train_loop.py
@@ -117,29 +117,30 @@ def train_loop(net,
         energy_losses_adv_total = list()
         glow_losses_total = list()
         for batch in tqdm(train_loader):
-            style_embedding = style_embedding_function(batch_of_spectrograms=batch[2].to(device),
-                                                       batch_of_spectrogram_lengths=batch[3].to(device))
-            for _ in range(100):
-                pitch_critic_loss, energy_critic_loss, duration_critic_loss = net.calculate_discriminator_loss(
-                    text_tensors=batch[0].to(device),
-                    text_lens=batch[1].to(device),
-                    gold_durations=batch[4].to(device),
-                    gold_pitch=batch[6].to(device),  # mind the switched order
-                    gold_energy=batch[5].to(device),  # mind the switched order
-                    utterance_embedding=style_embedding,
-                    lang_ids=batch[8].to(device),
-                )
-                loss = pitch_critic_loss + energy_critic_loss + duration_critic_loss
-                if use_wandb:
-                    wandb.log({
-                        "pitch_critic_loss"   : pitch_critic_loss.item(),
-                        "energy_critic_loss"  : energy_critic_loss.item(),
-                        "duration_critic_loss": duration_critic_loss.item(),
-                    })
-                optimizer.zero_grad()
-                loss.backward()
-                torch.nn.utils.clip_grad_norm_(net.parameters(), 1.0, error_if_nonfinite=False)
-                optimizer.step()
+            if step_counter > 1000:
+                style_embedding = style_embedding_function(batch_of_spectrograms=batch[2].to(device),
+                                                           batch_of_spectrogram_lengths=batch[3].to(device))
+                for _ in range(100):
+                    pitch_critic_loss, energy_critic_loss, duration_critic_loss = net.calculate_discriminator_loss(
+                        text_tensors=batch[0].to(device),
+                        text_lens=batch[1].to(device),
+                        gold_durations=batch[4].to(device),
+                        gold_pitch=batch[6].to(device),  # mind the switched order
+                        gold_energy=batch[5].to(device),  # mind the switched order
+                        utterance_embedding=style_embedding,
+                        lang_ids=batch[8].to(device),
+                    )
+                    loss = pitch_critic_loss + energy_critic_loss + duration_critic_loss
+                    if use_wandb:
+                        wandb.log({
+                            "pitch_critic_loss"   : pitch_critic_loss.item(),
+                            "energy_critic_loss"  : energy_critic_loss.item(),
+                            "duration_critic_loss": duration_critic_loss.item(),
+                        })
+                    optimizer.zero_grad()
+                    loss.backward()
+                    torch.nn.utils.clip_grad_norm_(net.parameters(), 1.0, error_if_nonfinite=False)
+                    optimizer.step()
             train_loss = 0.0
             with autocast():
                 if step_counter <= phase_1_steps:
