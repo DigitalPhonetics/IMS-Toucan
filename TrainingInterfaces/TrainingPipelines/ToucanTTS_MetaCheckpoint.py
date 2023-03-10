32c547bddeb134e3bc18546506f0b4c99b26e07a
Florian Lux <lux.florian@gmail.com>
2023-03-10 16:22:55 +0100

    switch meta checkpoint over to toucanTTS

--- TrainingInterfaces/TrainingPipelines/ToucanTTS_MetaCheckpoint.py
+++ TrainingInterfaces/TrainingPipelines/ToucanTTS_MetaCheckpoint.py
@@ -4,20 +4,16 @@ import torch
 import torch.multiprocessing
 import wandb
 from torch.utils.data import ConcatDataset
-from tqdm import tqdm
 
-from TrainingInterfaces.Spectrogram_to_Embedding.StyleEmbedding import StyleEmbedding
-from TrainingInterfaces.Text_to_Spectrogram.AutoAligner.AlignerDatasetBuilder import AlignerDatasetBuilder
-from TrainingInterfaces.Text_to_Spectrogram.PortaSpeech.PortaSpeech import PortaSpeech
-from TrainingInterfaces.Text_to_Spectrogram.PortaSpeech.portaspeech_train_loop_arbiter import train_loop
+from TrainingInterfaces.Text_to_Spectrogram.ToucanTTS.ToucanTTS import ToucanTTS
+from TrainingInterfaces.Text_to_Spectrogram.ToucanTTS.toucantts_train_loop_arbiter import train_loop
 from Utility.corpus_preparation import prepare_fastspeech_corpus
 from Utility.path_to_transcript_dicts import *
 from Utility.storage_config import MODELS_DIR
 from Utility.storage_config import PREPROCESSING_DIR
 
 
-def run(gpu_id, resume_checkpoint, finetune, model_dir, resume, use_wandb, wandb_resume_id, remove_faulty_samples=False,
-        generate_all_aligner_caches=False):
+def run(gpu_id, resume_checkpoint, finetune, model_dir, resume, use_wandb, wandb_resume_id):
     # It is not recommended training this yourself or to finetune this, but you can.
     # The recommended use is to download the pretrained model from the github release
     # page and finetune to your desired data
@@ -31,7 +27,7 @@ def run(gpu_id, resume_checkpoint, finetune, model_dir, resume, use_wandb, wandb
 
     datasets = list()
 
-    base_dir = os.path.join(MODELS_DIR, "PortaSpeech_Meta")
+    base_dir = os.path.join(MODELS_DIR, "ToucanTTS_Meta")
     if model_dir is not None:
         meta_save_dir = model_dir
     else:
@@ -39,9 +35,6 @@ def run(gpu_id, resume_checkpoint, finetune, model_dir, resume, use_wandb, wandb
     os.makedirs(meta_save_dir, exist_ok=True)
 
     print("Preparing")
-
-    if generate_all_aligner_caches:
-        build_all_aligner_dataset_caches(torch.device("cuda"))
 
     english_datasets = list()
     german_datasets = list()
@@ -66,10 +59,9 @@ def run(gpu_id, resume_checkpoint, finetune, model_dir, resume, use_wandb, wandb
                                                       corpus_dir=os.path.join(PREPROCESSING_DIR, "LJSpeech"),
                                                       lang="en"))
 
-    english_datasets.append(
-        prepare_fastspeech_corpus(transcript_dict=build_path_to_transcript_dict_libritts_all_clean(),
-                                  corpus_dir=os.path.join(PREPROCESSING_DIR, "libri_all_clean"),
-                                  lang="en"))
+    english_datasets.append(prepare_fastspeech_corpus(transcript_dict=build_path_to_transcript_dict_libritts_all_clean(),
+                                                      corpus_dir=os.path.join(PREPROCESSING_DIR, "libri_all_clean"),
+                                                      lang="en"))
 
     english_datasets.append(prepare_fastspeech_corpus(transcript_dict=build_path_to_transcript_dict_vctk(),
                                                       corpus_dir=os.path.join(PREPROCESSING_DIR, "vctk"),
@@ -116,10 +108,9 @@ def run(gpu_id, resume_checkpoint, finetune, model_dir, resume, use_wandb, wandb
                                                     corpus_dir=os.path.join(PREPROCESSING_DIR, "meta_Greek"),
                                                     lang="el"))
 
-    spanish_datasets.append(
-        prepare_fastspeech_corpus(transcript_dict=build_path_to_transcript_dict_spanish_blizzard_train(),
-                                  corpus_dir=os.path.join(PREPROCESSING_DIR, "spanish_blizzard"),
-                                  lang="es"))
+    spanish_datasets.append(prepare_fastspeech_corpus(transcript_dict=build_path_to_transcript_dict_spanish_blizzard_train(),
+                                                      corpus_dir=os.path.join(PREPROCESSING_DIR, "spanish_blizzard"),
+                                                      lang="es"))
 
     spanish_datasets.append(prepare_fastspeech_corpus(transcript_dict=build_path_to_transcript_dict_css10es(),
                                                       corpus_dir=os.path.join(PREPROCESSING_DIR, "meta_Spanish"),
@@ -204,28 +195,7 @@ def run(gpu_id, resume_checkpoint, finetune, model_dir, resume, use_wandb, wandb
     datasets.append(ConcatDataset(chinese_datasets))
     datasets.append(ConcatDataset(vietnamese_datasets))
 
-    if generate_all_aligner_caches:
-        return
-
-    if remove_faulty_samples:
-        find_and_remove_faulty_samples(net=PortaSpeech(),
-                                       datasets=english_datasets +
-                                                german_datasets +
-                                                greek_datasets +
-                                                spanish_datasets +
-                                                finnish_datasets +
-                                                russian_datasets +
-                                                hungarian_datasets +
-                                                dutch_datasets +
-                                                french_datasets +
-                                                portuguese_datasets +
-                                                polish_datasets +
-                                                italian_datasets +
-                                                chinese_datasets +
-                                                vietnamese_datasets,
-                                       device=torch.device("cuda"),
-                                       path_to_checkpoint=resume_checkpoint)
-    model = PortaSpeech()
+    model = ToucanTTS()
     if use_wandb:
         wandb.init(
             name=f"{__name__.split('.')[-1]}_{time.strftime('%Y%m%d-%H%M%S')}" if wandb_resume_id is None else None,
@@ -234,11 +204,7 @@ def run(gpu_id, resume_checkpoint, finetune, model_dir, resume, use_wandb, wandb
     train_loop(net=model,
                device=torch.device("cuda"),
                datasets=datasets,
-               batch_size=32,
-               # choosing batchsize for this is tricky.
-               # A large batchsize is dangerous for the VAE,
-               # but a large batchsize is better for the modelling of the languages.
-               # Starting from a single speaker single language model can help with this.
+               batch_size=16,
                save_directory=meta_save_dir,
                path_to_checkpoint=resume_checkpoint,
                path_to_embed_model=os.path.join(MODELS_DIR, "Embedding", "embedding_function.pt"),
@@ -247,164 +213,3 @@ def run(gpu_id, resume_checkpoint, finetune, model_dir, resume, use_wandb, wandb
                use_wandb=use_wandb)
     if use_wandb:
         wandb.finish()
-
-
-@torch.inference_mode()
-def find_and_remove_faulty_samples(net,
-                                   datasets,
-                                   device,
-                                   path_to_checkpoint,
-                                   path_to_embedding_checkpoint=os.path.join(MODELS_DIR, "Embedding",
-                                                                             "embedding_function.pt")):
-    net = net.to(device)
-    torch.multiprocessing.set_sharing_strategy('file_system')
-    check_dict = torch.load(os.path.join(path_to_checkpoint), map_location=device)
-    net.load_state_dict(check_dict["model"])
-    style_embedding_function = StyleEmbedding().to(device)
-    check_dict = torch.load(path_to_embedding_checkpoint, map_location=device)
-    style_embedding_function.load_state_dict(check_dict["style_emb_func"])
-    for dataset_index in range(len(datasets)):
-        nan_ids = list()
-        for datapoint_index in tqdm(range(len(datasets[dataset_index]))):
-            style_embedding = style_embedding_function(
-                batch_of_spectrograms=datasets[dataset_index][datapoint_index][2].unsqueeze(0).to(device),
-                batch_of_spectrogram_lengths=datasets[dataset_index][datapoint_index][3].to(device))
-            loss = net(text_tensors=datasets[dataset_index][datapoint_index][0].unsqueeze(0).to(device),
-                       text_lengths=datasets[dataset_index][datapoint_index][1].to(device),
-                       gold_speech=datasets[dataset_index][datapoint_index][2].unsqueeze(0).to(device),
-                       speech_lengths=datasets[dataset_index][datapoint_index][3].to(device),
-                       gold_durations=datasets[dataset_index][datapoint_index][4].unsqueeze(0).to(device),
-                       gold_pitch=datasets[dataset_index][datapoint_index][6].unsqueeze(0).to(device),
-                       # mind the switched order
-                       gold_energy=datasets[dataset_index][datapoint_index][5].unsqueeze(0).to(device),
-                       # mind the switched order
-                       utterance_embedding=style_embedding.unsqueeze(0).to(device),
-                       lang_ids=datasets[dataset_index][datapoint_index][8].unsqueeze(0).to(device),
-                       return_mels=False).squeeze()
-            if torch.isnan(loss):
-                print(f"NAN DETECTED: {dataset_index}, {datapoint_index}")
-                nan_ids.append(datapoint_index)
-        datasets[dataset_index].remove_samples(nan_ids)
-
-
-def build_all_aligner_dataset_caches(device):
-    factory = AlignerDatasetBuilder(device=device)
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_nancy(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "Nancy"),
-                        lang="en")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_ljspeech(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "LJSpeech"),
-                        lang="en")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_libritts_all_clean(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "libri_all_clean"),
-                        lang="en")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_vctk(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "vctk"),
-                        lang="en")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_nvidia_hifitts(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "hifi"),
-                        lang="en")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_RAVDESS(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "ravdess"),
-                        lang="en")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_ESDS(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "esds"),
-                        lang="en")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_karlsson(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "Karlsson"),
-                        lang="de")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_eva(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "Eva"),
-                        lang="de")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_hokus(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "Hokus"),
-                        lang="de")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_bernd(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "Bernd"),
-                        lang="de")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_hui_others(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "hui_others"),
-                        lang="de")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_thorsten(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "Thorsten"),
-                        lang="de")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_css10el(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "meta_Greek"),
-                        lang="el")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_spanish_blizzard_train(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "spanish_blizzard"),
-                        lang="es")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_css10es(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "meta_Spanish"),
-                        lang="es")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_mls_spanish(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "mls_spanish"),
-                        lang="es")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_css10fi(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "meta_Finnish"),
-                        lang="fi")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_css10ru(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "meta_Russian"),
-                        lang="ru")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_css10hu(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "meta_Hungarian"),
-                        lang="hu")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_css10nl(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "meta_Dutch"),
-                        lang="nl")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_mls_dutch(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "mls_dutch"),
-                        lang="nl")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_css10fr(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "meta_French"),
-                        lang="fr")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_mls_french(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "mls_french"),
-                        lang="fr")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_mls_portuguese(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "mls_porto"),
-                        lang="pt-br")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_mls_polish(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "mls_polish"),
-                        lang="pl")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_mls_italian(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "mls_italian"),
-                        lang="it")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_css10cmn(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "css10_chinese"),
-                        lang="cmn")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_aishell3(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "aishell3"),
-                        lang="cmn")
-
-    factory.build_cache(transcript_dict=build_path_to_transcript_dict_vietTTS(),
-                        corpus_dir=os.path.join(PREPROCESSING_DIR, "vietTTS"),
-                        lang="vi")
--- run_training_pipeline.py
+++ run_training_pipeline.py
@@ -8,9 +8,9 @@ from TrainingInterfaces.TrainingPipelines.JointEmbeddingFunction import run as e
 from TrainingInterfaces.TrainingPipelines.PortaSpeech_AD import run as ad
 from TrainingInterfaces.TrainingPipelines.PortaSpeech_French import run as french
 from TrainingInterfaces.TrainingPipelines.PortaSpeech_IntegrationTest import run as ps_integration_test
-from TrainingInterfaces.TrainingPipelines.PortaSpeech_MetaCheckpoint import run as meta
 from TrainingInterfaces.TrainingPipelines.PortaSpeech_NEB import run as neb
 from TrainingInterfaces.TrainingPipelines.ToucanTTS_Libri import run as libri
+from TrainingInterfaces.TrainingPipelines.ToucanTTS_MetaCheckpoint import run as meta
 from TrainingInterfaces.TrainingPipelines.ToucanTTS_Nancy import run as nancy
 from TrainingInterfaces.TrainingPipelines.finetuning_example import run as fine_tuning_example
 from TrainingInterfaces.TrainingPipelines.pretrain_aligner import run as aligner
