9249bd0646fbb598db7cb8768eee71f7e9483806
Florian Lux <lux.florian@gmail.com>
2023-03-16 22:55:04 +0100

    update test suite

--- TrainingInterfaces/TrainingPipelines/ToucanTTS_AD.py
+++ TrainingInterfaces/TrainingPipelines/ToucanTTS_AD.py
@@ -56,7 +56,7 @@ def run(gpu_id, resume_checkpoint, finetune, model_dir, resume, use_wandb, wandb
                resume=resume,
                warmup_steps=200,
                postnet_start_steps=200,
-               steps=4000,
+               steps=12000,
                use_wandb=use_wandb)
     if use_wandb:
         wandb.finish()
--- run_blizzard_test_suite.py
+++ run_blizzard_test_suite.py
@@ -26,9 +26,32 @@ def le_corbeau_et_le_renard(version, model_id="Meta", exec_device="cpu", speaker
         tts.read_to_file(text_list=[sentence], file_location=f"audios/{version}/Le_corbeau_et_le_renard_{i}.wav")
 
 
+def phonetically_interesting_sentences(version, model_id="Meta", exec_device="cpu", speaker_reference=None, vocoder_model_path=None, biggan=False):
+    os.makedirs("audios", exist_ok=True)
+    os.makedirs(f"audios/{version}", exist_ok=True)
+    tts = ToucanTTSInterface(device=exec_device, tts_model_path=model_id, vocoder_model_path=vocoder_model_path, faster_vocoder=not biggan)
+    tts.set_language("fr")
+    if speaker_reference is not None:
+        tts.set_utterance_embedding(speaker_reference)
+
+    for i, sentence in enumerate(["Les passagers, s'aidant les uns les autres, parvinrent à se dégager des mailles du filet.",
+                                  "On peut le faire si ce principe, vient en contrepartie d'un autre principe de même niveau.",
+                                  "Ce manuscrit, signé de mon nom, complété par l'histoire de ma vie, sera renfermé dans un petit appareil insubmersible.",
+                                  "On vous inviterait à chasser l'ours dans les montagnes de la Suisse, que vous diriez: «Très bien!»",
+                                  "Mais de là à monter cette histoire en épingle",
+                                  "La France mérite un tout autre projet.",
+                                  "Mon maître, dit alors nab, j'ai l'idée que nous pouvons chercher tant que nous voudrons le monsieur dont il s'agit, mais que nous ne le découvrirons que quand il lui plaira.",
+                                  "Pendant la première semaine du mois d'août, les rafales s'apaisèrent peu à peu, et l'atmosphère recouvra un calme qu'elle semblait avoir à jamais perdu.",
+                                  "Les amis ont vu un ancien ami en avril, dit-on.",
+                                  "Des amis ont vu en avril un vieil ami qui était très aimable, dit-on.",
+                                  "C'est une maison où l'on peut aller quand il pleut.",
+                                  "Après un tour de présentation, ils sont allés"
+                                  ]):
+        tts.read_to_file(text_list=[sentence], file_location=f"audios/{version}/test_sents_{i}.wav")
+
+
 if __name__ == '__main__':
     exec_device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"running on {exec_device}")
 
-    le_corbeau_et_le_renard(version="002_NEB_finetune", model_id="NEB", exec_device=exec_device, vocoder_model_path=None, biggan=True)
-    le_corbeau_et_le_renard(version="002_AD_finetune", model_id="AD", exec_device=exec_device, vocoder_model_path=None, biggan=True)
+    phonetically_interesting_sentences(version="002_AD_test", model_id="AD", exec_device=exec_device, vocoder_model_path=None, biggan=True)
