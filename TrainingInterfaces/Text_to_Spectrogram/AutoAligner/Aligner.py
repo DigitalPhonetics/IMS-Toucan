e41e266ccacf282a9854d562f9e3d604f1cf245b
Flux9665 <33219702+Flux9665@users.noreply.github.com>
2023-03-19 02:22:13 +0100

    Update Aligner.py

--- TrainingInterfaces/Text_to_Spectrogram/AutoAligner/Aligner.py
+++ TrainingInterfaces/Text_to_Spectrogram/AutoAligner/Aligner.py
@@ -108,6 +108,16 @@ class Aligner(torch.nn.Module):
                             tokens_indexed.append(self.tf.phone_to_id[phone])
                             # this is terribly inefficient, but it's fine
                             break
+                        elif vector[get_feature_to_index_lookup()["vowel"]] == 1 and vector[get_feature_to_index_lookup()["nasal"]] == 1:
+                            non_nasal_vowel = vector.cpu().numpy().tolist()
+                            non_nasal_vowel[get_feature_to_index_lookup()["nasal"]] = 0
+                            if non_nasal_vowel[13:] == self.tf.phone_to_vector[phone][13:]:
+                                # the first 12 dimensions are for modifiers, so we ignore those when trying to find the phoneme in the ID lookup
+                                # additionally we ignore the nasal flag for vowels, because unfortunately nasalized vowels exist.
+                                tokens_indexed.append(self.tf.phone_to_id[phone])
+                                # this is terribly inefficient, but it's fine
+                                break
+
             tokens = np.asarray(tokens_indexed)
         else:
             tokens = tokens.cpu().detach().numpy()
