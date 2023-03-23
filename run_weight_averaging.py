85333429be072c3f841be710c3e69e1e61819060
Thomas Bott <thomas_bott@outlook.de>
2023-03-23 15:57:52 +0100

    load default sentence embedding in averaging

--- run_weight_averaging.py
+++ run_weight_averaging.py
@@ -15,6 +15,7 @@ from Utility.storage_config import MODELS_DIR
 
 def load_net_porta(path):
     check_dict = torch.load(path, map_location=torch.device("cpu"))
+    default_sent_emb = None
     try:
         net = PortaSpeech()
         net.load_state_dict(check_dict["model"])
@@ -25,11 +26,12 @@ def load_net_porta(path):
         except RuntimeError:
             net = PortaSpeech(lang_embs=None, utt_embed_dim=None)
             net.load_state_dict(check_dict["model"])
-    return net, check_dict["default_emb"]
+    return net, check_dict["default_emb"], default_sent_emb
 
 
 def load_net_toucan(path):
     check_dict = torch.load(path, map_location=torch.device("cpu"))
+    default_sent_emb = None
     try:
         net = ToucanTTS()
         net.load_state_dict(check_dict["model"])
@@ -38,9 +40,19 @@ def load_net_toucan(path):
             net = ToucanTTS(lang_embs=None)
             net.load_state_dict(check_dict["model"])
         except RuntimeError:
-            net = ToucanTTS(lang_embs=None, utt_embed_dim=None)
-            net.load_state_dict(check_dict["model"])
-    return net, check_dict["default_emb"]
+            try:
+                net = ToucanTTS(lang_embs=None, utt_embed_dim=None)
+                net.load_state_dict(check_dict["model"])
+            except RuntimeError:
+                try:
+                    net = ToucanTTS(sent_embed_dim=768)
+                    net.load_state_dict(check_dict["model"])
+                    default_sent_emb = check_dict["default_sent_emb"]
+                except RuntimeError:
+                    net = ToucanTTS(lang_embs=None, sent_embed_dim=768)
+                    net.load_state_dict(check_dict["model"])
+                    default_sent_emb = check_dict["default_sent_emb"]
+    return net, check_dict["default_emb"], default_sent_emb
 
 
 def load_net_hifigan(path):
@@ -81,11 +93,12 @@ def average_checkpoints(list_of_checkpoint_paths, load_func):
     checkpoints_weights = {}
     model = None
     default_embed = None
+    default_sent_embed = None
 
     # LOAD CHECKPOINTS
     for path_to_checkpoint in list_of_checkpoint_paths:
         print("loading model {}".format(path_to_checkpoint))
-        model, default_embed = load_func(path=path_to_checkpoint)
+        model, default_embed, default_sent_embed = load_func(path=path_to_checkpoint)
         checkpoints_weights[path_to_checkpoint] = dict(model.named_parameters())
 
     # AVERAGE CHECKPOINTS
@@ -105,17 +118,20 @@ def average_checkpoints(list_of_checkpoint_paths, load_func):
     model_dict.update(dict_params)
     model.load_state_dict(model_dict)
     model.eval()
-    return model, default_embed
+    return model, default_embed, default_sent_embed
 
 
-def save_model_for_use(model, name="", default_embed=None, dict_name="model"):
+def save_model_for_use(model, name="", default_embed=None, default_sent_embed=None, dict_name="model"):
     print("saving model...")
     if default_embed is None:
         # HiFiGAN case
         torch.save({dict_name: model.state_dict()}, name)
-    else:
-        # TTS case
+    elif default_sent_embed is None:
+        # TTS case without sentence embedding
         torch.save({dict_name: model.state_dict(), "default_emb": default_embed}, name)
+    else:
+        # TTS case with sentence embedding
+        torch.save({dict_name: model.state_dict(), "default_emb": default_embed, "default_sent_emb": default_sent_embed}, name)
     print("...done!")
 
 
@@ -146,8 +162,8 @@ def make_best_in_all():
                 checkpoint_paths = get_n_recent_checkpoints_paths(checkpoint_dir=os.path.join(MODELS_DIR, model_dir), n=3)
                 if checkpoint_paths is None:
                     continue
-                averaged_model, default_embed = average_checkpoints(checkpoint_paths, load_func=load_net_toucan)
-                save_model_for_use(model=averaged_model, default_embed=default_embed, name=os.path.join(MODELS_DIR, model_dir, "best.pt"))
+                averaged_model, default_embed, default_sent_embed = average_checkpoints(checkpoint_paths, load_func=load_net_toucan)
+                save_model_for_use(model=averaged_model, default_embed=default_embed, default_sent_embed=default_sent_embed, name=os.path.join(MODELS_DIR, model_dir, "best.pt"))
 
 
 def count_parameters(net):
