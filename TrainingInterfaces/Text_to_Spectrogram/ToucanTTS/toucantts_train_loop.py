fix weight averaging

TrainingInterfaces/Text_to_Spectrogram/ToucanTTS/toucantts_train_loop.py
@@ -246,8 +246,8 @@ def train_loop(net,
         if step_counter > postnet_start_steps * 1.5:
             # Run manual SWA (torch builtin doesn't work unfortunately due to the use of weight norm in the postflow)
             checkpoint_paths = get_n_recent_checkpoints_paths(checkpoint_dir=save_directory, n=3)
-            averaged_model, default_embed, default_sent_embed = average_checkpoints(checkpoint_paths, load_func=load_net_toucan)
-            save_model_for_use(model=averaged_model, default_embed=default_embed, default_sent_embed=default_sent_embed, name=os.path.join(save_directory, "best.pt"))
+            averaged_model, default_embed, default_sent_embed, default_word_embed = average_checkpoints(checkpoint_paths, load_func=load_net_toucan)
+            save_model_for_use(model=averaged_model, default_embed=default_embed, default_sent_embed=default_sent_embed, default_word_embed=default_word_embed, name=os.path.join(save_directory, "best.pt"))
             check_dict = torch.load(os.path.join(save_directory, "best.pt"), map_location=device)
             net.load_state_dict(check_dict["model"])
 