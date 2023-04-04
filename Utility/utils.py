8d382decfb4afba27f62eccb282f1b08cf2e2ec5
flux9665 <lux.florian@gmail.com>
2023-04-04 16:41:26 +0200

    update model downloader for release

--- Utility/utils.py
+++ Utility/utils.py
@@ -16,7 +16,6 @@ from matplotlib.lines import Line2D
 import Layers.ConditionalLayerNorm
 from Preprocessing.TextFrontend import ArticulatoryCombinedTextFrontend
 from Preprocessing.TextFrontend import get_language_id
-from Preprocessing.WordEmbeddingExtractor import WordEmbeddingExtractor
 
 
 def make_estimated_durations_usable_for_inference(xs, offset=1.0):
@@ -193,11 +192,6 @@ def plot_progress_spec_toucantts(net,
     sentence = tf.get_example_sentence(lang=lang)
     if sentence is None:
         return None
-    if default_word_emb is not None:
-        # change default word emb since it should fit the test sentence
-        word_embedding_extractor = WordEmbeddingExtractor()
-        word_embeddings, _ = word_embedding_extractor.encode([sentence])
-        default_word_emb = word_embeddings.squeeze()
     phoneme_vector = tf.string_to_tensor(sentence).squeeze(0).to(device)
     if run_postflow:
         spec_before, spec_after, durations, pitch, energy = net.inference(text=phoneme_vector,
