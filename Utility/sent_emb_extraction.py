77d663aca26a7178d6e32ee4eb41b3d2d73e9761
Thomas Bott <thomas_bott@outlook.de>
2023-04-19 16:01:19 +0200

    extract promptspeech prompts

--- Utility/sent_emb_extraction.py
+++ Utility/sent_emb_extraction.py
@@ -1,10 +1,17 @@
 from tqdm import tqdm
 
-def extract_sent_embs(train_set, sent_emb_extractor):
+def extract_sent_embs(train_set, sent_emb_extractor, promptspeech=False):
     sent_embs = {}
     print("Extracting sentence embeddings.")
+    if promptspeech:
+        from Utility.path_to_transcript_dicts import build_sent_to_prompt_dict_promptspeech
+        sent_to_prompt_dict = build_sent_to_prompt_dict_promptspeech()
     for index in tqdm(range(len(train_set))):
         sentence = train_set[index][9]
-        sent_emb = sent_emb_extractor.encode(sentences=[sentence]).squeeze()
+        if promptspeech:
+            prompt = sent_to_prompt_dict[sentence]
+            sent_emb = sent_emb_extractor.encode(sentences=[prompt]).squeeze()
+        else:
+            sent_emb = sent_emb_extractor.encode(sentences=[sentence]).squeeze()
         sent_embs[sentence] = sent_emb
     return sent_embs
