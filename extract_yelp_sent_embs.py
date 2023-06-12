ac7c550a1b6609880ea13a6f9629ea412337be0f
Thomas Bott <thomas_bott@outlook.de>
2023-06-12 15:00:37 +0200

    update extraction

--- extract_yelp_sent_embs.py
+++ extract_yelp_sent_embs.py
@@ -5,12 +5,12 @@ import os
 from tqdm import tqdm
 
 if __name__ == '__main__':
-    device = 'cuda:0'
-    emotion_prompts = torch.load(os.path.join(PREPROCESSING_DIR, "Yelp", "emotion_prompts_large.pt"), map_location='cpu')
+    device = 'cuda:4'
+    emotion_prompts = torch.load(os.path.join(PREPROCESSING_DIR, "Yelp", "emotion_prompts_balanced_10000.pt"), map_location='cpu')
     sent_emb_extractor = SentenceEmbeddingExtractor(pooling='cls', device=device)
     emotion_prompts_sent_embs = {"anger":[], "disgust":[], "fear":[], "joy":[], "neutral":[], "sadness":[], "surprise":[]}
     for emotion in tqdm(list(emotion_prompts.keys())):
         for prompt in tqdm(emotion_prompts[emotion]):
             sent_emb = sent_emb_extractor.encode(sentences=[prompt]).squeeze()
             emotion_prompts_sent_embs[emotion].append(sent_emb)
-    torch.save(emotion_prompts_sent_embs, os.path.join(PREPROCESSING_DIR, "Yelp", "emotion_prompts_large_sent_embs_emoBERT.pt"))
+    torch.save(emotion_prompts_sent_embs, os.path.join(PREPROCESSING_DIR, "Yelp", "emotion_prompts_balanced_10000_sent_embs_emoBERTcls.pt"))
