0ec67f1cb22df17b486425b44abdf9819fd08fa8
flux9665 <lux.florian@gmail.com>
2023-06-15 13:01:24 +0200

    reduce batchsize again to save the fileserver

--- TrainingInterfaces/TrainingPipelines/AlignerPipeline.py
+++ TrainingInterfaces/TrainingPipelines/AlignerPipeline.py
@@ -294,7 +294,7 @@ def run(gpu_id, resume_checkpoint, finetune, model_dir, resume, use_wandb, wandb
                   device=device,
                   save_directory=save_dir,
                   steps=500000,
-                  batch_size=256,
+                  batch_size=64,
                   path_to_checkpoint=resume_checkpoint,
                   fine_tune=finetune,
                   debug_img_path=save_dir_aligner,
