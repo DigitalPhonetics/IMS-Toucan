407616201cdd9ac2f5b9dea68933449cfb59cfdc
Florian Lux <lux.florian@gmail.com>
2023-06-29 15:20:08 +0200

    simplified speech reconstruction in the aligner again

--- TrainingInterfaces/Text_to_Spectrogram/AutoAligner/SpectrogramReconstructor.py
+++ TrainingInterfaces/Text_to_Spectrogram/AutoAligner/SpectrogramReconstructor.py
@@ -1,7 +1,5 @@
 import torch
 import torch.multiprocessing
-from torch.nn.utils.rnn import pack_padded_sequence
-from torch.nn.utils.rnn import pad_packed_sequence
 
 from Utility.utils import make_non_pad_mask
 
@@ -12,22 +10,20 @@ class SpectrogramReconstructor(torch.nn.Module):
                  n_mels=1024,
                  num_symbols=145,
                  speaker_embedding_dim=192,
-                 lstm_dim=512):
+                 hidden_dim=128):
         super().__init__()
-        self.in_proj = torch.nn.Linear(num_symbols + speaker_embedding_dim, lstm_dim)
-        self.rnn1 = torch.nn.LSTM(lstm_dim, lstm_dim, batch_first=True, bidirectional=True)
-        self.rnn2 = torch.nn.LSTM(2 * lstm_dim, lstm_dim, batch_first=True, bidirectional=True)
-        self.out_proj = torch.nn.Linear(2 * lstm_dim, n_mels)
+        self.spectral_reconstruction = torch.nn.Sequential(torch.nn.Linear(num_symbols + speaker_embedding_dim, hidden_dim),
+                                                           torch.nn.Tanh(),
+                                                           torch.nn.Linear(hidden_dim, hidden_dim),
+                                                           torch.nn.Tanh(),
+                                                           torch.nn.Linear(hidden_dim, hidden_dim),
+                                                           torch.nn.Tanh(),
+                                                           torch.nn.Linear(hidden_dim, n_mels))
         self.l1_criterion = torch.nn.L1Loss(reduction="none")
         self.l2_criterion = torch.nn.MSELoss(reduction="none")
 
     def forward(self, x, lens, ys):
-        x = self.in_proj(x)
-        x = pack_padded_sequence(x, lens.cpu(), batch_first=True, enforce_sorted=False)
-        x, _ = self.rnn1(x)
-        x, _ = self.rnn2(x)
-        x, _ = pad_packed_sequence(x, batch_first=True)
-        x = self.out_proj(x)
+        x = self.spectral_reconstruction(x)
         out_masks = make_non_pad_mask(lens).unsqueeze(-1).to(ys.device)
         out_weights = out_masks.float() / out_masks.sum(dim=1, keepdim=True).float()
         out_weights /= ys.size(0) * ys.size(2)
