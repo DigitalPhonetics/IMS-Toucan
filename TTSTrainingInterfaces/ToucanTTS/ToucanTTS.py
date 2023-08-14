fix averaging

TTSTrainingInterfaces/ToucanTTS/ToucanTTS.py
@@ -250,10 +250,10 @@ class ToucanTTS(torch.nn.Module):
         self.backtranslation_heads = torch.nn.ModuleList()
         self.padding_id = self.codebook_size + 5
         for head in range(self.num_codebooks):
-            self.hierarchical_classifier.append(torch.nn.Sequential(torch.nn.Dropout(.2),
+            self.hierarchical_classifier.append(torch.nn.Sequential(torch.nn.Dropout(.1),
                                                                     torch.nn.Linear(attention_dimension + head * backtranslation_dim, attention_dimension),
                                                                     torch.nn.Tanh(),
-                                                                    torch.nn.Dropout(.2),
+                                                                    torch.nn.Dropout(.1),
                                                                     torch.nn.Linear(attention_dimension, self.codebook_size)))
             self.backtranslation_heads.append(torch.nn.Embedding(num_embeddings=self.padding_id + 1, embedding_dim=backtranslation_dim, padding_idx=self.padding_id))
 

run_text_to_file_reader.py
@@ -44,6 +44,6 @@ if __name__ == '__main__':
     exec_device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"running on {exec_device}")
 
-    the_raven(version="MetaBaseline",
-              model_id="Meta",
+    the_raven(version="CodecNancy_v1",
+              model_id="Nancy",
               exec_device=exec_device)

run_weight_averaging.py
@@ -12,7 +12,7 @@ from Utility.storage_config import MODELS_DIR
 
 def load_net_toucan(path):
     check_dict = torch.load(path, map_location=torch.device("cpu"))
-    net = ToucanTTS(config=check_dict["config"], weights=check_dict["model"])
+    net = ToucanTTS(weights=check_dict["model"], config=check_dict["config"])
     return net, check_dict["default_emb"]
 
 