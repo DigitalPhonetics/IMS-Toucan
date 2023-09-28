58e7623d6bc1cf73b3a07b278455c6d5647de4f3
Florian Lux <lux.florian@gmail.com>
2023-09-28 13:51:56 +0200

    minor fix

--- Utility/utils.py
+++ Utility/utils.py
@@ -380,11 +380,11 @@ def curve_smoother(curve):
 
 def remove_elements(tensor, indexes):
     # Create a boolean mask where True represents the elements to keep
-    mask = torch.ones_like(tensor, dtype=torch.bool)
+    mask = torch.ones(tensor.size(0), dtype=torch.bool)
     mask[indexes] = False
 
     # Use the mask to select the elements to keep
-    result = tensor[mask]
+    result = tensor[mask, :]
 
     return result
 
