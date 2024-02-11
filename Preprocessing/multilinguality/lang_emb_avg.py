fa642f1a22dcd2d4146c25cc88edcd223598701b
Lyonel Behringer <lyonel.behringer@iis.fraunhofer.de>
2024-02-11 10:55:33 +0100

    make langemb retrieval faster, replace hardcoded k closest languages by variable k

--- Preprocessing/multilinguality/lang_emb_avg.py
+++ Preprocessing/multilinguality/lang_emb_avg.py
@@ -4,23 +4,28 @@ import os
 import numpy as np
 import pandas as pd
 sys.path.append("/home/behringe/hdd_behringe/IMS-Toucan")
-from Preprocessing.TextFrontend import get_language_id
+import json
 
 
-def compute_mse_for_averaged_embeddings(csv_path, language_embeddings, weighted_avg=False):
+
+def compute_mse_for_averaged_embeddings(csv_path, iso_lookup, language_embeddings, weighted_avg=False):
     dataset_df = pd.read_csv(csv_path, sep="|")
 
     loss_fn = torch.nn.MSELoss()
     running_loss = 0.
 
     # for combined feats, df has 5 features per closest lang + 1 target lang column
-    if "average_dist_0" in dataset_df.columns or "euclidean_dist_0" in dataset_df.columns:
+    if "average_dist_0" in dataset_df.columns and "map_dist_0" in dataset_df.columns or "euclidean_dist_0" in dataset_df.columns and "map_dist_0" in dataset_df.columns:
         n_closest = len(dataset_df.columns) // 5
         distance_type = "average" if "average_dist_0" in dataset_df.columns else "euclidean"
     # else, df has 2 features per closest lang + 1 target lang column
     else:
         n_closest = len(dataset_df.columns) // 2
-        if "map_dist_0" in dataset_df.columns:
+        if "average_dist_0" in dataset_df.columns:
+            distance_type = "average"
+        elif "euclidean_dist_0" in dataset_df.columns:
+            distance_type = "euclidean"
+        elif "map_dist_0" in dataset_df.columns:
             distance_type = "map"
         elif "tree_dist_0" in dataset_df.columns:
             distance_type = "tree"
@@ -28,31 +33,24 @@ def compute_mse_for_averaged_embeddings(csv_path, language_embeddings, weighted_
             distance_type = "asp"
         else:
             distance_type = "fixed" # for random dataset
-    assert n_closest == 5
+
+    closest_lang_columns = [f"closest_lang_{i}" for i in range(n_closest)]
+    closest_dist_columns = [f"{distance_type}_dist_{i}" for i in range(n_closest)]
 
     for row in dataset_df.itertuples():
-        y = language_embeddings[get_language_id(row.target_lang).item()]
+        y = language_embeddings[iso_lookup[-1][row.target_lang]]
         avg_emb = torch.zeros([16])
-        langs = [row.closest_lang_0, row.closest_lang_1, row.closest_lang_2, row.closest_lang_3, row.closest_lang_4]
-        if distance_type == "average":
-            dists = [row.average_dist_0, row.average_dist_1, row.average_dist_2, row.average_dist_3, row.average_dist_4]
-        elif distance_type == "asp":
-            dists = [row.asp_dist_0, row.asp_dist_1, row.asp_dist_2, row.asp_dist_3, row.asp_dist_4]
-        elif distance_type == "map":
-            dists = [row.map_dist_0, row.map_dist_1, row.map_dist_2, row.map_dist_3, row.map_dist_4]
-        elif distance_type == "tree":
-            dists = [row.tree_dist_0, row.tree_dist_1, row.tree_dist_2, row.tree_dist_3, row.tree_dist_4]
-        else:
-            dists = [row.fixed_dist_0, row.fixed_dist_1, row.fixed_dist_2, row.fixed_dist_3, row.fixed_dist_4]
+        langs = [getattr(row, l) for l in closest_lang_columns]
+        dists = [getattr(row, d) for d in closest_dist_columns]
 
         if weighted_avg:
             for lang, dist in zip(langs, dists):
-                lang_emb = language_embeddings[get_language_id(lang).item()]
+                lang_emb = language_embeddings[iso_lookup[-1][lang]]
                 avg_emb += lang_emb * dist
             normalization_factor = sum(dists)
         else:
             for lang in langs:
-                lang_emb = language_embeddings[get_language_id(lang).item()]
+                lang_emb = language_embeddings[iso_lookup[-1][lang]]
                 avg_emb += lang_emb
             normalization_factor = len(langs)
         avg_emb /= normalization_factor # normalize
@@ -80,14 +78,42 @@ if __name__ == "__main__":
         #"datasets/dataset_tree_463_with_less_loss_fixed_tree_distance.csv",
         #"datasets/dataset_random_463_with_less_loss_fixed_tree_distance.csv",
 
+        # 10-100 kNN, with individual dists
+        #"datasets/dataset_COMBINED_463_with_less_loss_fixed_tree_distance_top10_average_individual_dists.csv",
+        #"datasets/dataset_COMBINED_463_with_less_loss_fixed_tree_distance_top20_average_individual_dists.csv",
+        #"datasets/dataset_COMBINED_463_with_less_loss_fixed_tree_distance_top25_average_individual_dists.csv",
+        #"datasets/dataset_COMBINED_463_with_less_loss_fixed_tree_distance_top30_average_individual_dists.csv",
+        #"datasets/dataset_COMBINED_463_with_less_loss_fixed_tree_distance_top40_average_individual_dists.csv",        
+        #"datasets/dataset_COMBINED_463_with_less_loss_fixed_tree_distance_top50_average_individual_dists.csv",
+        #"datasets/dataset_COMBINED_463_with_less_loss_fixed_tree_distance_top100_average_individual_dists.csv"
+
+        # RANDOM
+        #"datasets/dataset_random_463_with_less_loss_fixed_tree_distance_random20.csv",
+        #"datasets/dataset_random_463_with_less_loss_fixed_tree_distance_random25.csv",
+        #"datasets/dataset_random_463_with_less_loss_fixed_tree_distance_random30.csv",
+
+        # LEARNED WEIGHTS (with/without individual dists)
+
+        "datasets/dataset_COMBINED_463_with_less_loss_fixed_tree_distance_average_individual_dists_learned_weights.csv",
+        "datasets/dataset_COMBINED_463_with_less_loss_fixed_tree_distance_top20_average_individual_dists_learned_weights.csv",
+        "datasets/dataset_COMBINED_463_with_less_loss_fixed_tree_distance_top25_average_individual_dists_learned_weights.csv",
+        "datasets/dataset_COMBINED_463_with_less_loss_fixed_tree_distance_top30_average_individual_dists_learned_weights.csv",
+        #"datasets/dataset_COMBINED_463_with_less_loss_fixed_tree_distance_average_learned_weights.csv",        
+        # "datasets/dataset_COMBINED_463_with_less_loss_fixed_tree_distance_top20_average_learned_weights.csv",
+        # "datasets/dataset_COMBINED_463_with_less_loss_fixed_tree_distance_top25_average_learned_weights.csv",
+        # "datasets/dataset_COMBINED_463_with_less_loss_fixed_tree_distance_top30_average_learned_weights.csv",
+
     ]
     weighted = [True, False]
-    #lang_embs_path = "LangEmbs/final_model_with_less_loss.pt"
     lang_embs_path = "LangEmbs/final_model_with_less_loss_fixed_tree_distance.pt"
     language_embeddings = torch.load(lang_embs_path)
+
+    ISO_LOOKUP_PATH = "iso_lookup.json"
+    with open(ISO_LOOKUP_PATH, "r") as f:
+        iso_lookup = json.load(f)
 
     for csv_path in csv_paths:
         print(f"csv_path: {csv_path}")
         for condition in weighted:
-            avg_loss = compute_mse_for_averaged_embeddings(csv_path, language_embeddings, condition)
+            avg_loss = compute_mse_for_averaged_embeddings(csv_path, iso_lookup, language_embeddings, condition)
             print(f"weighted average: {condition} | avg loss: {avg_loss}")
