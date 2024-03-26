eb63d2275e7ea26c7b6c34395900ed0a12f24ecf
Florian Lux <lux.florian@gmail.com>
2024-03-26 11:23:54 +0100

    implement first version of the demo

--- run_GUI_demo.py
+++ run_GUI_demo.py
@@ -1,12 +1,17 @@
 import gradio as gr
 
 from InferenceInterfaces.ControllableInterface import ControllableInterface
+from Preprocessing.multilinguality.SimilaritySolver import load_json_from_path
 from Utility.utils import float2pcm
 
 
 class TTSWebUI:
 
-    def __init__(self, gpu_id="cpu", title="Text-to-Speech for over 7000 Languages", article="", available_artificial_voices=1000):
+    def __init__(self, gpu_id="cpu", title="Controllable Text-to-Speech for over 7000 Languages", article="", available_artificial_voices=1000, path_to_iso_list="Preprocessing/multilinguality/iso_to_fullname.json"):
+        iso_to_name = load_json_from_path(path_to_iso_list)
+        text_selection = [f"{iso_to_name[iso_code]} Text ({iso_code})" for iso_code in iso_to_name]
+        accent_selection = [f"{iso_to_name[iso_code]} Accent ({iso_code})" for iso_code in iso_to_name]
+
         self.controllable_ui = ControllableInterface(gpu_id=gpu_id,
                                                      available_artificial_voices=available_artificial_voices)
         self.iface = gr.Interface(fn=self.read,
@@ -14,57 +19,27 @@ class TTSWebUI:
                                                      placeholder="write what you want the synthesis to read here...",
                                                      value="Colorless green ideas sleep furiously!",
                                                      label="Text input"),
-                                          gr.Dropdown(['English Text',
-                                                       'German Text',
-                                                       'Greek Text',
-                                                       'Spanish Text',
-                                                       'Finnish Text',
-                                                       'Russian Text',
-                                                       'Hungarian Text',
-                                                       'Dutch Text',
-                                                       'French Text',
-                                                       'Polish Text',
-                                                       'Portuguese Text',
-                                                       'Italian Text',
-                                                       'Chinese Text',
-                                                       'Vietnamese Text'], type="value", value='English Text', label="Select the Language of the Text"),
-                                          gr.Dropdown(['English Accent',
-                                                       'German Accent',
-                                                       'Greek Accent',
-                                                       'Spanish Accent',
-                                                       'Finnish Accent',
-                                                       'Russian Accent',
-                                                       'Hungarian Accent',
-                                                       'Dutch Accent',
-                                                       'French Accent',
-                                                       'Polish Accent',
-                                                       'Portuguese Accent',
-                                                       'Italian Accent',
-                                                       'Chinese Accent',
-                                                       'Vietnamese Accent'], type="value",
-                                                      value='English Accent', label="Select the Accent of the Speaker"),
-                                          gr.Textbox(lines=3,
-                                                     placeholder="\nThe sliders below control the speaker embedding",
-                                                     value="\nThe sliders below control the speaker embedding",
-                                                     label=" ",
-                                                     show_label=False),
+                                          gr.Dropdown(text_selection,
+                                                      type="value",
+                                                      value='English Text (eng)',
+                                                      label="Select the Language of the Text (type on your keyboard to find it quickly)"),
+                                          gr.Dropdown(accent_selection,
+                                                      type="value",
+                                                      value='English Accent (eng)',
+                                                      label="Select the Accent of the Speaker (type on your keyboard to find it quickly)"),
                                           gr.Slider(minimum=0, maximum=available_artificial_voices, step=1,
                                                     value=279,
                                                     label="Random Seed for the artificial Voice"),
-                                          gr.Slider(minimum=-50.0, maximum=50.0, step=0.1, value=0.0, label="Femininity / Masculinity"),
-                                          gr.Slider(minimum=-30.0, maximum=30.0, step=0.1, value=0.0, label="Sibilance"),
-                                          gr.Slider(minimum=-30.0, maximum=30.0, step=0.1, value=0.0, label="Accentuated High / Low Frequencies"),
-                                          gr.Slider(minimum=-30.0, maximum=30.0, step=0.1, value=0.0, label="Loudness / Arousal / Calmness"),
-                                          gr.Slider(minimum=-20.0, maximum=20.0, step=0.1, value=0.0, label="Tone / Timbre"),
-                                          gr.Textbox(lines=3,
-                                                     placeholder="\nThe sliders below directly control the TTS",
-                                                     value="\nThe sliders below directly control the TTS",
-                                                     label=" ",
-                                                     show_label=False),
                                           gr.Slider(minimum=0.5, maximum=1.5, step=0.1, value=1.0, label="Duration Scale"),
                                           gr.Slider(minimum=0.0, maximum=2.0, step=0.1, value=1.0, label="Pause Duration Scale"),
                                           gr.Slider(minimum=0.0, maximum=2.0, step=0.1, value=1.0, label="Pitch Variance Scale"),
-                                          gr.Slider(minimum=0.0, maximum=2.0, step=0.1, value=1.0, label="Energy Variance Scale")
+                                          gr.Slider(minimum=0.0, maximum=2.0, step=0.1, value=1.0, label="Energy Variance Scale"),
+                                          gr.Slider(minimum=-50.0, maximum=50.0, step=0.1, value=0.0, label="Femininity / Masculinity"),
+                                          gr.Slider(minimum=-30.0, maximum=30.0, step=0.1, value=0.0, label="???"),
+                                          gr.Slider(minimum=-30.0, maximum=30.0, step=0.1, value=0.0, label="???"),
+                                          gr.Slider(minimum=-30.0, maximum=30.0, step=0.1, value=0.0, label="???"),
+                                          gr.Slider(minimum=-30.0, maximum=30.0, step=0.1, value=0.0, label="???"),
+                                          gr.Slider(minimum=-20.0, maximum=20.0, step=0.1, value=0.0, label="???"),
                                           ],
                                   outputs=[gr.Audio(type="numpy", label="Speech"),
                                            gr.Image(label="Visualization")],
@@ -78,21 +53,21 @@ class TTSWebUI:
              prompt,
              language,
              accent,
-             ignore_1,
              voice_seed,
-             emb1,
-             emb2,
-             emb3,
-             emb5,
-             emb6,
-             ignore_2,
              duration_scaling_factor,
              pause_duration_scaling_factor,
              pitch_variance_scale,
-             energy_variance_scale):
+             energy_variance_scale,
+             emb1,
+             emb2,
+             emb3,
+             emb4,
+             emb5,
+             emb6
+             ):
         sr, wav, fig = self.controllable_ui.read(prompt,
-                                                 language,
-                                                 accent,
+                                                 language[-4:-1],
+                                                 accent[-4:-1],
                                                  voice_seed,
                                                  duration_scaling_factor,
                                                  pause_duration_scaling_factor,
@@ -101,7 +76,7 @@ class TTSWebUI:
                                                  emb1,
                                                  emb2,
                                                  emb3,
-                                                 0.0,  # slider 4 did not have a meaningful interpretation, too many properties mixed
+                                                 emb4,
                                                  emb5,
                                                  emb6)
         return (sr, float2pcm(wav)), fig
