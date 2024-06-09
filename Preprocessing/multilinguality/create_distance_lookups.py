d5a0439b57a25834d360255c36a9bac820fd6320
Lyonel Behringer <lyonel.behringer@hotmail.de>
2024-06-09 12:22:12 +0200

    prepare zero-shot approximation for release

--- Preprocessing/multilinguality/create_distance_lookups.py
+++ Preprocessing/multilinguality/create_distance_lookups.py
@@ -22,7 +22,7 @@ class CacheCreator:
 
         self.pair_to_tree_similarity = dict()
         self.pair_to_depth = dict()
-        for pair in tqdm(self.pairs):
+        for pair in tqdm(self.pairs, desc="Generating tree pairs"):
             self.pair_to_tree_similarity[pair] = len(set(iso_to_family_memberships[pair[0]]).intersection(set(iso_to_family_memberships[pair[1]])))
             self.pair_to_depth[pair] = len(iso_to_family_memberships[pair[0]]) + len(iso_to_family_memberships[pair[1]])
         
@@ -43,7 +43,7 @@ class CacheCreator:
     def create_map_cache(self, cache_root="."):
         self.pair_to_map_dist = dict()
         iso_to_long_lat = load_json_from_path(os.path.join(cache_root, "iso_to_long_lat.json"))
-        for pair in tqdm(self.pairs):
+        for pair in tqdm(self.pairs, desc="Generating map pairs"):
             try:
                 long_1, lat_1 = iso_to_long_lat[pair[0]]
                 long_2, lat_2 = iso_to_long_lat[pair[1]]
@@ -64,31 +64,34 @@ class CacheCreator:
             json.dump(lang_1_to_lang_2_to_map_dist, f, ensure_ascii=False, indent=4)
 
     def create_oracle_cache(self, model_path, cache_root="."):
-        """Oracle language-embedding distance of supervised languages is only used for evaluation, not usable for zero-shot."""
+        """Oracle language-embedding distance of supervised languages is only used for evaluation, not usable for zero-shot.
+        
+        Note: The generated oracle cache is only valid for the given `model_path`!"""
         loss_fn = torch.nn.MSELoss(reduction="mean")
-        self.pair_to_lang_emb_dist = dict()
+        self.pair_to_oracle_dist = dict()
         lang_embs = torch.load(model_path)["model"]["encoder.language_embedding.weight"]
         lang_embs.requires_grad_(False)
-        for pair in tqdm(self.pairs):
+        for pair in tqdm(self.pairs, desc="Generating oracle pairs"):
             try:
                 dist = loss_fn(lang_embs[self.iso_lookup[-1][pair[0]]], lang_embs[self.iso_lookup[-1][pair[1]]]).item()
-                self.pair_to_lang_emb_dist[pair] = dist
+                self.pair_to_oracle_dist[pair] = dist
             except KeyError:
                 pass
-        lang_1_to_lang_2_lang_emb_dist = dict()
-        for pair in self.pair_to_lang_emb_dist:
+        lang_1_to_lang_2_oracle_dist = dict()
+        for pair in self.pair_to_oracle_dist:
             lang_1 = pair[0]
             lang_2 = pair[1]
-            dist = self.pair_to_lang_emb_dist[pair]
-            if lang_1 not in lang_1_to_lang_2_lang_emb_dist.keys():
-                lang_1_to_lang_2_lang_emb_dist[lang_1] = dict()
-            lang_1_to_lang_2_lang_emb_dist[lang_1][lang_2] = dist         
+            dist = self.pair_to_oracle_dist[pair]
+            if lang_1 not in lang_1_to_lang_2_oracle_dist.keys():
+                lang_1_to_lang_2_oracle_dist[lang_1] = dict()
+            lang_1_to_lang_2_oracle_dist[lang_1][lang_2] = dist         
         with open(os.path.join(cache_root, "lang_1_to_lang_2_to_oracle_dist.json"), "w", encoding="utf-8") as f:
-            json.dump(lang_1_to_lang_2_lang_emb_dist, f, ensure_ascii=False, indent=4)
+            json.dump(lang_1_to_lang_2_oracle_dist, f, ensure_ascii=False, indent=4)
 
     def create_learned_cache(self, model_path, cache_root="."):
+        """Note: The generated learned distance cache is only valid for the given `model_path`!"""
         # TODO
-        raise NotImplementedError("currently located in MetricMetaLearner.py")
+        raise NotImplementedError("currently handled in MetricMetaLearner.py")
 
     def create_required_files(self, model_path=None, create_oracle=False):
         if not os.path.exists("lang_1_to_lang_2_to_tree_dist.json"):
