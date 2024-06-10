bb4755be932a5b4423c309804d0158b2c6757b7a
Flux9665 <33219702+Flux9665@users.noreply.github.com>
2024-06-10 15:51:36 +0200

    Update run_model_downloader.py

--- run_model_downloader.py
+++ run_model_downloader.py
@@ -67,7 +67,7 @@ def download_models():
     print("Downloading Baseline Model (Prompting)")
     os.makedirs(os.path.join(MODELS_DIR, "ToucanTTS_Baseline"), exist_ok=True)
     filename, headers = urllib.request.urlretrieve(
-        url="https://github.com/Thommy96/IMS-Toucan/releases/download/v1.0/baseline.pt",
+        url="https://github.com/DigitalPhonetics/IMS-Toucan/releases/download/v2.p/baseline.pt",
         filename=os.path.abspath(os.path.join(MODELS_DIR, "ToucanTTS_Baseline", "best.pt")),
         reporthook=report)
     
@@ -75,7 +75,7 @@ def download_models():
     print("Downloading Proposed Model (Prompting)")
     os.makedirs(os.path.join(MODELS_DIR, "ToucanTTS_Proposed"), exist_ok=True)
     filename, headers = urllib.request.urlretrieve(
-        url="https://github.com/Thommy96/IMS-Toucan/releases/download/v1.0/proposed.pt",
+        url="https://github.com/DigitalPhonetics/IMS-Toucan/releases/download/v2.p/proposed.pt",
         filename=os.path.abspath(os.path.join(MODELS_DIR, "ToucanTTS_Proposed", "best.pt")),
         reporthook=report)
 
