fa64ca62ea5e6024dea918eb18f78e27b35d6bbe
Florian Lux <flux@apptek.com>
2025-05-17 19:29:36 +0200

    make a symbol replacement to account for Hindi punctuation

--- Preprocessing/TextFrontend.py
+++ Preprocessing/TextFrontend.py
@@ -780,6 +780,8 @@ class ArticulatoryCombinedTextFrontend:
     def get_phone_string(self, text, include_eos_symbol=True, for_feature_extraction=False, for_plot_labels=False):
         if text == "":
             return ""
+        text = text.replace("|", "."), # Hindi uses this symbol to indicate the end of a sentence.
+
         # expand abbreviations
         utt = self.expand_abbreviations(text)
 
